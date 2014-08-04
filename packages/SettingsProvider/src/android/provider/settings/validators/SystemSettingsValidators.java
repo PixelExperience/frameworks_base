960d32b9d9a8510fd13117b83a805f8fc81d5bda
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (2/2).
    Adapted to Lineage SDK and squashed with the following changes:
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 10:40:41 -0700
    
          Keyguard: Don't disable visibility when scrambling pin.
    
            Otherwise the first numbers text is flipped to invisible
            even though it shouldn't be.
    
          Change-Id: Ia2b80d594e7f11af7a60a85bdb4ea9909d0ac20b
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 16:21:27 -0700
    
          Keyguard: Fix scramblepin logic.
    
          Change-Id: I71e6d26e853fa7bfaeea4b3256902881ac7f74fc
    
    Original Change-Id: Ibe8611eb0b8a5bd9275bc96682d2cbafaa37d8f5
    Change-Id: I6716b1c76b69d51bd1457ce07de2274541fca1e8
    
    Change-Id: I1ac16b1f77fa3d8caaf8a74d6da458f10148c969

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -241,5 +241,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.NAVIGATION_BAR_MENU_ARROW_KEYS, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.STATUS_BAR_QUICK_QS_PULLDOWN, new InclusiveIntegerRangeValidator(0, 2));
         VALIDATORS.put(System.DOUBLE_TAP_SLEEP_GESTURE, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, BOOLEAN_VALIDATOR);
     }
 }
