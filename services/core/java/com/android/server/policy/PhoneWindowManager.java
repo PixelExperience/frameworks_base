26bd4eb23451c2621d5dec651e3b20e243e7cd19
Carlo Savignano <carlosavignano@aospa.co>
2016-10-23 18:41:27 +0800

    pocket: introduce pocket judge
    * Judge if device is in pocket.
    * Notify clients callbacks when pocked state changes.
    * Start listening when device becomes not interactive.
    * Stop listening when device becomes interactive and is NOT in pocket.
    
    Ticket: NOUGAT-9.
    
    Change-Id: Ifcf59c172f55cce95a72411820723696ef2dd9ae
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -223,6 +223,7 @@ import android.os.UEventObserver;
 import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.pocket.PocketManager;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.service.dreams.DreamManagerInternal;
@@ -912,6 +913,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
     private VolumeKeyHandler mVolumeKeyHandler;
     private AlternativeDeviceKeyHandler mAlternativeDeviceKeyHandler;
+
+    private PocketManager mPocketManager;
 
     private static final int MSG_ENABLE_POINTER_LOCATION = 1;
     private static final int MSG_DISABLE_POINTER_LOCATION = 2;
@@ -7779,6 +7782,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mKeyguardDelegate != null) {
             mKeyguardDelegate.onStartedGoingToSleep(why);
         }
+        if (mPocketManager != null) {
+            mPocketManager.onInteractiveChanged(false);
+        }
     }
 
     // Called on the PowerManager's Notifier thread.
@@ -7826,6 +7832,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         if (mKeyguardDelegate != null) {
             mKeyguardDelegate.onStartedWakingUp();
+        }
+
+        if (mPocketManager != null) {
+            mPocketManager.onInteractiveChanged(true);
         }
     }
 
@@ -8586,6 +8596,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // In normal flow, systemReady is called before other system services are ready.
         // So it is better not to bind keyguard here.
         mKeyguardDelegate.onSystemReady();
+
+        mPocketManager = (PocketManager) mContext.getSystemService(Context.POCKET_SERVICE);
 
         mVrManagerInternal = LocalServices.getService(VrManagerInternal.class);
         if (mVrManagerInternal != null) {
