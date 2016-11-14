2f7d2610c5366498cdf5c6f36a1fda6796f00e39
George G <kreach3r@users.noreply.github.com>
2016-11-14 14:49:47 +0200

    Notification dynamic colors bool
    forward ported to oreo
    
    OMS7 introduced this fine piece of code: https://github.com/SubstratumResources/platform_frameworks_base/blob/n-oms7/core/java/android/app/ResourcesManager.java#L897..#L904
    
    // Resources.getSystem Resources are created on request and aren't tracked by
    // mResourceReferences.
    //
    // If overlays targeting "android" are to be used, we must create the system
    // resources regardless of whether they already exist, since otherwise the
    // information on what overlays to use would be lost. This is wasteful for most
    // applications, so limit this operation to the system user only. (This means
    // Resources.getSystem() will *not* use overlays for applications.)
    
    Replaced deprecated Resources.getSystem() with compatible method.
    
    Change-Id: I02efe27de3cc7067552964ffbaf079f9e9b5bc3e

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4824,7 +4824,7 @@ public class Notification implements Parcelable
                 boolean ambient) {
             boolean colorable = !isLegacy() || getColorUtil().isGrayscaleIcon(mContext, smallIcon);
             int color;
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
                 color = ambient ? resolveAmbientColor() : mContext.getColor(R.color.notification_icon_default_color);
             } else {
                 color = ambient ? resolveAmbientColor() : getPrimaryHighlightColor();
@@ -4863,7 +4863,7 @@ public class Notification implements Parcelable
         }
 
         int resolveIconContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
                 return mContext.getColor(R.color.notification_icon_default_color);
             } else {
                 return resolveContrastColor();
@@ -4871,7 +4871,7 @@ public class Notification implements Parcelable
         }
 
         int resolveContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
                 return mContext.getColor(R.color.notification_text_default_color);
             }
 
