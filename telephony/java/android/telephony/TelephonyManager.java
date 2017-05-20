f93791c3e585e61cf02f93a509df630558467fcd
Simao Gomes Viana <xdevs23@outlook.com>
2017-05-20 16:30:54 +0200

    TelephonyManager: add some inconsistent methods
    Some proprietary blobs might try to invoke these,
    but it won't work because they don't exist.
    
    So I'm adding them here and hoping it will fix that problem.
    
    Change-Id: Ic05c54ac75abf566324468481d6376091a409a7e
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1181,6 +1181,13 @@ public class TelephonyManager {
     }
 
     /**
+     * @hide
+     */
+    public CellLocation getCellLocationBySubId(int subId) {
+        return getCellLocation();
+    }
+
+    /**
      * Enables location update notifications.  {@link PhoneStateListener#onCellLocationChanged
      * PhoneStateListener.onCellLocationChanged} will be called on location updates.
      *
@@ -1607,6 +1614,13 @@ public class TelephonyManager {
         CarrierConfigManager carrierConfigManager = mContext
                 .getSystemService(CarrierConfigManager.class);
         return carrierConfigManager.getConfigForSubId(getSubId());
+    }
+
+    /**
+     * @hide
+     */
+    public String getNetworkOperatorForSubscription(int subId) {
+        return getNetworkOperatorForPhone(subId);
     }
 
     /**
