e335d8e4ce27e679abddddad58ca8e429e18bee1
Alberto97 <albertop2197@gmail.com>
2017-10-14 20:41:23 +0200

    SystemUI: Port brightness slider changes
     * Expose AutoBrightness button
     * Add tunables
     * Use vector drawables for brightness icons
    
    Author: Luca Stefani <luca020400@lineageos.org>
    Date:   Thu Apr 20 18:00:52 2017 +0200
    
        SystemUI: Use AOSP AutoBrightness implementation
    
        Change-Id: I3e3660065407d460f30c6e936d951d4d5eae30c6
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Thu Apr 27 14:57:49 2017 +0300
    
        SystemUI: Use already existing overlay to show QS auto brightness
    
        * No need for a new overlay when one exists for years
    
        Change-Id: I9e09a1770f8e645093e880cf1a48597405e0bcf2
    
    Author: Alberto97 <albertop2197@gmail.com>
    Date:   Mon May 29 21:07:05 2017 +0200
    
        SystemUI: Specify user while changing brightness mode
    
        Allow any user changing brightness mode,
        without this only the main one is able to do that.
    
        BUGBASH-598
    
        Change-Id: Ib51e9a45c42837cabb1cfb0d48334165b023a827
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Apr 30 13:45:39 2017 +0300
    
        SystemUI: Add tunable for auto brightness icon
    
        Change-Id: I02fe3f5500f95ba9369b952bbec2a073b7ae8316
    
    Author: Alexander Westphal <westphal.alex94@gmail.com>
    Date:   Wed Apr 26 09:20:53 2017 +0100
    
        SystemUI: use vector drawables for brightness icons
    
        Change-Id: I8d22f8de22040d68ed1bfab0da3a78aec717fa62
    
    Author: qjohn <townex22@gmail.com>
    Date:   Sun Jun 18 16:28:10 2017 +0700
    
        SystemUI: Remove unnecessary brightness icons
    
        Change-Id: I7e60ff73125bd0304f9ca14ad5c5a9ce11ffceda
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Mon May 25 15:38:33 2020 +0200
    
        SystemUI: Brightness slider: Always show the brightness icon
    
        * Right now, we reserve the space for the icon but it's not displayed
          (reserved space is there so the slider doesn't jump)
        * Always display the icon (if automatic brightness is available)
          so it's more consistent
    
        Change-Id: I5238dc6aad51407349a8d42fae840ea76841dc3d
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Fri May 29 20:09:18 2020 +0200
    
        SystemUI: Fix for auto brightness icon being shown when disabled
    
        * With commit 9d42c48c16bba2fcc836cbb68e2979ed9c0d1b23 the case of a
          manually disabled auto brightness icon got lost
        * Re-implement it by also respecting the Settings value instead of just
          the build time flag
    
        Change-Id: I991977be7e1f7c33947073dbd3ce7a800fb158bf
    
    Change-Id: Ic72b0084a4108fb4fa26e893bd6d1eaca96cbb6c

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -30,11 +30,13 @@ import android.metrics.LogMaker;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.android.internal.logging.MetricsLogger;
@@ -77,7 +79,10 @@ import javax.inject.Named;
 public class QSPanel extends LinearLayout implements Tunable, Callback, BrightnessMirrorListener,
         Dumpable {
 
-    public static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+    private static final String QS_SHOW_AUTO_BRIGHTNESS =
+            "customsecure:" + Settings.Secure.QS_SHOW_AUTO_BRIGHTNESS;
+    public static final String QS_SHOW_BRIGHTNESS_SLIDER =
+            "customsecure:" + Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER;
     public static final String QS_SHOW_HEADER = "qs_show_header";
 
     private static final String TAG = "QSPanel";
@@ -95,6 +100,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     @Nullable
     protected View mBrightnessView;
+    protected ImageView mAutoBrightnessView;
     @Nullable
     private BrightnessController mBrightnessController;
 
@@ -108,6 +114,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     protected boolean mExpanded;
     protected boolean mListening;
+    private boolean mIsAutomaticBrightnessAvailable = false;
 
     private QSDetail.Callback mCallback;
     private final DumpManager mDumpManager;
@@ -229,7 +236,12 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             R.layout.quick_settings_brightness_dialog, this, false);
         addView(mBrightnessView);
         mBrightnessController = new BrightnessController(getContext(),
+                findViewById(R.id.brightness_icon),
                 findViewById(R.id.brightness_slider), mBroadcastDispatcher);
+        mAutoBrightnessView = findViewById(R.id.brightness_icon);
+
+        mIsAutomaticBrightnessAvailable = getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
     }
 
     protected QSTileLayout createRegularTileLayout() {
@@ -335,7 +347,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         final TunerService tunerService = Dependency.get(TunerService.class);
-        tunerService.addTunable(this, QS_SHOW_BRIGHTNESS);
+        tunerService.addTunable(this, QS_SHOW_AUTO_BRIGHTNESS);
+        tunerService.addTunable(this, QS_SHOW_BRIGHTNESS_SLIDER);
 
         if (mHost != null) {
             setTiles(mHost.getTiles());
@@ -377,7 +390,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (QS_SHOW_BRIGHTNESS.equals(key) && mBrightnessView != null) {
+        if (QS_SHOW_AUTO_BRIGHTNESS.equals(key) && mIsAutomaticBrightnessAvailable) {
+            updateViewVisibilityForTuningValue(mAutoBrightnessView, newValue);
+        } else if (QS_SHOW_BRIGHTNESS_SLIDER.equals(key) && mBrightnessView != null) {
             updateViewVisibilityForTuningValue(mBrightnessView, newValue);
         }
     }
