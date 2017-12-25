7d9fd60f3bf1a2774c3ce3e54a9ea67393ffcf1a
LuK1337 <priv.luk@gmail.com>
2017-12-25 16:54:07 +0100

    Implement volume wake
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Mon Sep 26 00:43:08 2016 +0200
    
        Fix volume keys wakeup status handling
    
        The same status flag is used for the three different volume keys,
        however nothing prevents users from pressing multiple keys at the
        same time. This allows to set the status flag with one volume key
        and clear it with the other volume key.
    
        Use one flag per key so that we never end up in an inconsistent
        state. This fixes the seldom power button issues that happen when
        the "volume wake" feature is enabled.
    
        Change-Id: I08f5f9ff696bef3dd840cff97d570e44ebe03e4e
    
    Author: Martin Brabham <optedoblivion@cyngn.com>
    Date:   Wed Dec 3 11:48:28 2014 -0800
    
        Android Policy: handle volume key event as wake key when preference is set
    
        Change-Id: If9a61cd65553bf00f0efda1a75b1ab75b9129090
    
    Author: Matt Garnes <matt@cyngn.com>
    Date:   Tue Mar 31 14:39:38 2015 -0700
    
        If a wake key is disabled by the user, do not wake from doze.
    
        Currently, any wake key will wake the device from a doze, even if that
        key has not been enabled as a wake key in Settings.
    
        If the device is 'dreaming' in the Doze state, check if the user has
        explicitly disabled the wake key (or never enabled the setting in the
        first place) before waking the device.
    
        Change-Id: I7397087c143161e8e1ddb84d0e23f6027fea0aac
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Thu Dec 18 04:26:38 2014 +0200
    
        Cleanup button wake settings (2/2)
    
        Change-Id: Ie37136cbd57c4c334321abbfa4543727e940bc43
    
        Keep quiet when volume keys are used to wake up device
    
        - Userspace will make a 'beep' with it receives a key up, so
          consume that event as well.
        - Removed wake key check in music control code as it will already
          be disabled here.
    
    Change-Id: I93839acd39aec8a2ee40291f833a31f6b048c9f8
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -236,5 +236,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.NAVIGATION_BAR_MENU_ARROW_KEYS, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.STATUS_BAR_QUICK_QS_PULLDOWN, new InclusiveIntegerRangeValidator(0, 2));
         VALIDATORS.put(System.DOUBLE_TAP_SLEEP_GESTURE, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.VOLUME_WAKE_SCREEN, BOOLEAN_VALIDATOR);
     }
 }
