b703e49e6a5f3e2f40e551d928da2639139b8c8e
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2017-12-28 21:55:45 +0100

    Allow screen unpinning on devices without navbar
    Change-Id: Iedfc08f4d95bbee3c8578c0d2450b90739e63603
    
    Screen Pinning: Show correct text for on screen nav.
    
      Similar to I09c2ef661bff272cb4f7ca43bac0e45f4b20a4d4,
      we're not getting an instance of PhoneWindowManager which
      we can rely on to update dynamically.
    
      TICKET: OPO-393
    
    Change-Id: Iacf8221066461fb6940dd88432e665812545c3ff

--- packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
+++ packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
@@ -42,6 +42,7 @@ import android.text.style.BulletSpan;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.IWindowManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -85,6 +86,7 @@ public class ScreenPinningRequest implements View.OnClickListener,
     private final WindowManager mWindowManager;
     private final BroadcastDispatcher mBroadcastDispatcher;
     private final UserTracker mUserTracker;
+    private final IWindowManager mWindowManagerService;
 
     private RequestWindowView mRequestWindow;
     private int mNavBarMode;
@@ -116,6 +118,7 @@ public class ScreenPinningRequest implements View.OnClickListener,
         mNavBarMode = navigationModeController.addListener(this);
         mBroadcastDispatcher = broadcastDispatcher;
         mUserTracker = userTracker;
+        mWindowManagerService = WindowManagerGlobal.getWindowManagerService();
     }
 
     public void clearPrompt() {
@@ -300,14 +303,18 @@ public class ScreenPinningRequest implements View.OnClickListener,
                 mLayout.findViewById(R.id.screen_pinning_recents_group).setVisibility(VISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(INVISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(INVISIBLE);
-                descriptionStringResId = touchExplorationEnabled
+                descriptionStringResId = !hasNavigationBar()
+                        ? R.string.screen_pinning_description_no_navbar
+                        : touchExplorationEnabled
                         ? R.string.screen_pinning_description_accessible
                         : R.string.screen_pinning_description;
             } else {
                 mLayout.findViewById(R.id.screen_pinning_recents_group).setVisibility(INVISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(VISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(VISIBLE);
-                descriptionStringResId = touchExplorationEnabled
+                descriptionStringResId = !hasNavigationBar()
+                        ? R.string.screen_pinning_description_no_navbar
+                        : touchExplorationEnabled
                         ? R.string.screen_pinning_description_recents_invisible_accessible
                         : R.string.screen_pinning_description_recents_invisible;
             }
@@ -390,6 +397,15 @@ public class ScreenPinningRequest implements View.OnClickListener,
                     linearLayout.addView(childList.get(i));
                 }
             }
+        }
+
+        private boolean hasNavigationBar() {
+            try {
+                return mWindowManagerService.hasNavigationBar(mContext.getDisplayId());
+            } catch (RemoteException e) {
+                // ignore
+            }
+            return false;
         }
 
         @Override
