8d10b59fff0f337166faa8a976176d2a2b3cd5d9
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2017-12-28 21:55:45 +0100

    Allow screen unpinning on devices without navbar
    Change-Id: Iedfc08f4d95bbee3c8578c0d2450b90739e63603
    
    Screen Pinning: Show correct text for on screen nav.
    
      Similar to I09c2ef661bff272cb4f7ca43bac0e45f4b20a4d4,
      we're not getting an instance of PhoneWindowManager which
      we can rely on to update dynamically.
    
      TICKET: OPO-393
    
    Change-Id: Iacf8221066461fb6940dd88432e665812545c3ff

--- packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
+++ packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
@@ -35,9 +35,11 @@ import android.text.SpannableStringBuilder;
 import android.text.style.BulletSpan;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.IWindowManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 import android.view.accessibility.AccessibilityManager;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
@@ -72,6 +74,7 @@ public class ScreenPinningRequest implements View.OnClickListener,
     private final AccessibilityManager mAccessibilityService;
     private final WindowManager mWindowManager;
     private final OverviewProxyService mOverviewProxyService;
+    private final IWindowManager mWindowManagerService;
 
     private RequestWindowView mRequestWindow;
     private int mNavBarMode;
@@ -89,6 +92,7 @@ public class ScreenPinningRequest implements View.OnClickListener,
                 mContext.getSystemService(Context.WINDOW_SERVICE);
         mOverviewProxyService = Dependency.get(OverviewProxyService.class);
         mNavBarMode = Dependency.get(NavigationModeController.class).addListener(this);
+        mWindowManagerService = WindowManagerGlobal.getWindowManagerService();
     }
 
     public void clearPrompt() {
@@ -276,14 +280,18 @@ public class ScreenPinningRequest implements View.OnClickListener,
                 mLayout.findViewById(R.id.screen_pinning_recents_group).setVisibility(VISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(INVISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(INVISIBLE);
-                descriptionStringResId = touchExplorationEnabled
+                descriptionStringResId = !hasNavigationBar()
+                        ? R.string.screen_pinning_description_no_navbar
+                        : touchExplorationEnabled
                         ? R.string.screen_pinning_description_accessible
                         : R.string.screen_pinning_description;
             } else {
                 mLayout.findViewById(R.id.screen_pinning_recents_group).setVisibility(INVISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(VISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(VISIBLE);
-                descriptionStringResId = touchExplorationEnabled
+                descriptionStringResId = !hasNavigationBar()
+                        ? R.string.screen_pinning_description_no_navbar
+                        : touchExplorationEnabled
                         ? R.string.screen_pinning_description_recents_invisible_accessible
                         : R.string.screen_pinning_description_recents_invisible;
             }
@@ -333,6 +341,15 @@ public class ScreenPinningRequest implements View.OnClickListener,
                     linearLayout.addView(childList.get(i));
                 }
             }
+        }
+
+        private boolean hasNavigationBar() {
+            try {
+                return mWindowManagerService.hasNavigationBar(mContext.getDisplayId());
+            } catch (RemoteException e) {
+                // ignore
+            }
+            return false;
         }
 
         @Override
