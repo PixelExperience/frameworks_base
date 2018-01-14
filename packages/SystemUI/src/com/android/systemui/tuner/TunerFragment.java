c7d10aab11c3694bd5e927438251ce81fe31b468
Pranav Vashi <neobuddy89@gmail.com>
2018-01-14 14:10:00 +0530

    base: Disable AOD by default [2/2]
    Change-Id: Ie736c598bbeeddacb6c32aef2b6e2f7c78b9c828

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -78,7 +78,7 @@ public class TunerFragment extends PreferenceFragment {
     }
 
     private boolean alwaysOnAvailable() {
-        return new AmbientDisplayConfiguration(getContext()).alwaysOnAvailable();
+        return false;
     }
 
     @Override
