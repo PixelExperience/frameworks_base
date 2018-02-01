eae27e213dcaa25beda831641a8c2e073ecc8e02
Dabug123 <clarkbrunswick55@gmail.com>
2018-02-01 14:53:52 +0100

    frameworks:more performance
    Change-Id: Ie4089164618ba075c3d6d1b8a8d564b79e064460

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4610,7 +4610,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
 
         FlingRunnable() {
             mScroller = new OverScroller(getContext());
-            mScroller.setFriction(0.006f);
+            mScroller.setFriction(0.004f);
         }
 
         void start(int initialVelocity) {
