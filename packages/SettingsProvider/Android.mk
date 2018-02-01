a78c16136043387fa83d30d06e10feb03f05cbc6
Dan Pasanen <dan.pasanen@gmail.com>
2018-02-01 11:18:00 -0600

    SettingsProvider: explicitly include android support libs
    * Fixes non-jack build
    
    Change-Id: Id95700b06228e2c322ffdb1395ff9d28eb8813ad

--- packages/SettingsProvider/Android.mk
+++ packages/SettingsProvider/Android.mk
@@ -7,7 +7,13 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
     src/com/android/providers/settings/EventLogTags.logtags
 
 LOCAL_JAVA_LIBRARIES := telephony-common ims-common
-LOCAL_STATIC_JAVA_LIBRARIES := junit legacy-android-test
+LOCAL_STATIC_JAVA_LIBRARIES := junit legacy-android-test \
+    android-support-annotations \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-recyclerview \
+    android-support-v7-preference \
+    android-support-v14-preference
 
 LOCAL_PACKAGE_NAME := SettingsProvider
 LOCAL_CERTIFICATE := platform
