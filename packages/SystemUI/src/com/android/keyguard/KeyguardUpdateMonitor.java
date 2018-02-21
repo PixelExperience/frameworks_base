97bacd7c9ad242399c91a46fa39202ee6c9488db
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-02-21 20:59:54 -0300

    BatteryService: add turbo power charging support
    Inspired by oneplus dash charging commit
    
    https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    
    Change-Id: I0cdd47255291fb3f560770cfaeda304eb49f5a14

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -29,6 +29,7 @@ import static android.os.BatteryManager.EXTRA_MAX_CHARGING_VOLTAGE;
 import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_STATUS;
 import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
+import static android.os.BatteryManager.EXTRA_TURBO_POWER;
 
 import android.annotation.AnyThread;
 import android.annotation.MainThread;
@@ -806,9 +807,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
                     maxChargingMicroWatt = -1;
                 }
                 final boolean dashChargeStatus = intent.getBooleanExtra(EXTRA_DASH_CHARGER, false);
+                final boolean turboPowerStatus = intent.getBooleanExtra(EXTRA_TURBO_POWER, false);
                 final Message msg = mHandler.obtainMessage(
                         MSG_BATTERY_UPDATE, new BatteryStatus(status, level, plugged, health,
-                                maxChargingMicroWatt, dashChargeStatus));
+                                maxChargingMicroWatt, dashChargeStatus, turboPowerStatus));
                 mHandler.sendMessage(msg);
             } else if (TelephonyIntents.ACTION_SIM_STATE_CHANGED.equals(action)) {
                 // ACTION_SIM_STATE_CHANGED is rebroadcast after unlocking the device to
@@ -999,6 +1001,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         public static final int CHARGING_REGULAR = 1;
         public static final int CHARGING_FAST = 2;
         public static final int CHARGING_DASH = 3;
+        public static final int CHARGING_TURBO_POWER = 4;
 
         public final int status;
         public final int level;
@@ -1006,14 +1009,16 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         public final int health;
         public final int maxChargingWattage;
         public final boolean dashChargeStatus;
+        public final boolean turboPowerStatus;
         public BatteryStatus(int status, int level, int plugged, int health,
-                int maxChargingWattage, boolean dashChargeStatus) {
+                int maxChargingWattage, boolean dashChargeStatus, boolean turboPowerStatus) {
             this.status = status;
             this.level = level;
             this.plugged = plugged;
             this.health = health;
             this.maxChargingWattage = maxChargingWattage;
             this.dashChargeStatus = dashChargeStatus;
+            this.turboPowerStatus = turboPowerStatus;
         }
 
         /**
@@ -1055,6 +1060,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
 
         public final int getChargingSpeed(int slowThreshold, int fastThreshold) {
             return dashChargeStatus ? CHARGING_DASH :
+                    turboPowerStatus ? CHARGING_TURBO_POWER :
                     maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                     maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                     maxChargingWattage > fastThreshold ? CHARGING_FAST :
@@ -1203,7 +1209,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, false);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, false, false);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -1727,6 +1733,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
 
         // change in dash charging while plugged in
         if (nowPluggedIn && current.dashChargeStatus != old.dashChargeStatus) {
+            return true;
+        }
+
+        // change in turbo power charging while plugged in
+        if (nowPluggedIn && current.turboPowerStatus != old.turboPowerStatus) {
             return true;
         }
 
