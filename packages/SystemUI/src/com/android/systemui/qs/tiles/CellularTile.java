331f3a32daa14b3e9e59a1b7023a5435f318ff19
Harsh Shandilya <harsh@prjkt.io>
2018-04-18 16:14:09 +0530

    CellularTile: Avoid NPE during boot phase
    * Listening handling can only happen after mController
      is properly initialised.
    
    Change-Id: I64f32b1eacbf5dffc9702a39c95f2baf33b99d2c
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -93,6 +93,7 @@ public class CellularTile extends QSTileImpl<SignalState> {
 
     @Override
     public void handleSetListening(boolean listening) {
+        if (mController == null) return;
         if (listening) {
             mController.addCallback(mSignalCallback);
         } else {
@@ -157,6 +158,7 @@ public class CellularTile extends QSTileImpl<SignalState> {
 
     @Override
     protected void handleUpdateState(SignalState state, Object arg) {
+        if (mSignalCallback == null) return;
         CallbackInfo cb = (CallbackInfo) arg;
         if (cb == null) {
             cb = mSignalCallback.mInfo;
