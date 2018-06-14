13477ca6893124a11b588996f82c53bfc3c5aaa2
ezio84 <brabus84@gmail.com>
2018-06-14 13:19:44 +0200

    Remove useless dream manager isDozing check
    This check was introduced by this:
    https://github.com/ezio84/abc_frameworks_base/commit/31884d13a2d0d352f58206811314f4167674116d
    
    but now it's not needed anymore
    
    Change-Id: I75dbabec4876e924081faacd6a03ffbe775c5671

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1549,7 +1549,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean isDozeMode() {
         IDreamManager dreamManager = getDreamManager();
         try {
-            if (dreamManager != null && dreamManager.isDozing()) {
+            if (dreamManager != null && dreamManager.isDreaming()) {
                 return true;
             }
         } catch (RemoteException e) {
@@ -6920,15 +6920,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return false;
         }
 
-        IDreamManager dreamManager = getDreamManager();
-        try {
-            if (dreamManager != null && dreamManager.isDozing()) {
-                if (event != null && isVolumeKey(event)) {
-                    return false;
-                }
+        boolean isDozing = isDozeMode();
+
+        if (isDozing) {
+            if (event != null && isVolumeKey(event)) {
+                return false;
             }
-        } catch (RemoteException e) {
-            Slog.e(TAG, "RemoteException when checking if dreaming", e);
         }
 
         // Send events to keyguard while the screen is on and it's showing.
@@ -6946,12 +6943,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         // Send events to a dozing dream even if the screen is off since the dream
         // is in control of the state of the screen.
-        try {
-            if (dreamManager != null && dreamManager.isDreaming()) {
-                return true;
-            }
-        } catch (RemoteException e) {
-            Slog.e(TAG, "RemoteException when checking if dreaming", e);
+        if (isDozing) {
+            return true;
         }
 
         // Otherwise, consume events since the user can't see what is being
