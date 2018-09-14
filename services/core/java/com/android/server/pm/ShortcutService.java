f3a37d68e2d4562bc4fa28df2372b59bc0dad1d9
maxwen <max.weninger@gmail.com>
2018-09-14 17:35:55 -0500

    Use custom fingerprint for ShortcutService
    make sure cached data gets updated
    no more derped deep shortcuts text and images
    
    Change-Id: I65d743a4991f92739e59b4655dcc086bd36a5401

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -4262,7 +4262,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     // Injection point.
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return Build.CUSTOM_FINGERPRINT;
     }
 
     final void wtf(String message) {
