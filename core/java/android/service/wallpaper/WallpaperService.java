f4cba150fce94247155a3343eaebb77a68b110ad
jianhuac <jianhuac@codeaurora.org>
2018-09-17 18:11:02 +0800

    WallpaperService: Fix half black wallpaper after rotating quickly
    When rotate the device quickly in a transparent application activity,
    sometime the wallpaper will be half black.
    
    Root cause:
    Sometime the transaction that change the size of the wallpaper surface
    behind the drawFrame of wallpaper.
    
    Redraw the frame of wallpaper one more time only when wallpaper
    size changed can fix it.
    
    Change-Id: I0884d0a6b51691f41e031092a2ef698c7e3a21a5
    CRs-Fixed: 2287784
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/android/service/wallpaper/WallpaperService.java
+++ core/java/android/service/wallpaper/WallpaperService.java
@@ -2590,7 +2590,7 @@ public abstract class WallpaperService extends Service {
                     return;
                 }
                 case MSG_UPDATE_SURFACE:
-                    mEngine.updateSurface(true, false, false);
+                    mEngine.updateSurface(true, false, true/*false*/);
                     break;
                 case MSG_ZOOM:
                     mEngine.setZoom(Float.intBitsToFloat(message.arg1));
