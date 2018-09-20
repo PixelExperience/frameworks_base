30bf8e15af90d956b837096a394416a1664eea35
ezio84 <brabus84@gmail.com>
2018-09-20 07:49:28 -0500

    Unload old SystemUI dark overlay
    - Be sure the old stock dark theme is disabled if the user kept it
    enabled before, otherwise SystemUI will be always dark themed even
    after switching to light mode.
    
    Change-Id: I3df81c1b344b1e9eadd1f120afc243d393e5d0b5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2204,6 +2204,20 @@ public class StatusBar extends SystemUI implements DemoMode,
         return themeInfo != null && themeInfo.isEnabled();
     }
 
+    public void unloadStockDarkTheme() {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.systemui.theme.dark",
+                    mLockscreenUserManager.getCurrentUserId());
+            if (themeInfo != null && themeInfo.isEnabled()) {
+                mOverlayManager.setEnabled("com.android.systemui.theme.dark",
+                        false /*disable*/, mLockscreenUserManager.getCurrentUserId());
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Nullable
     public View getAmbientIndicationContainer() {
         return mAmbientIndicationContainer;
@@ -4025,6 +4039,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                             useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                     mOverlayManager.setEnabled("com.android.settings.theme.dark",
                             useDarkTheme, mLockscreenUserManager.getCurrentUserId());
+                    if (useDarkTheme) {
+                        unloadStockDarkTheme();
+                    }
                 } catch (RemoteException e) {
                     Log.w(TAG, "Can't change theme", e);
                 }
