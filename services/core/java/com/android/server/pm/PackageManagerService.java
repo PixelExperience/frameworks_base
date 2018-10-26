d8035f7f64f9208287128025bdae6a5b9a0d87bc
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-26 13:54:13 -0300

    PackageManagerService: Ignore vendor overlays that must be on system/app
    Change-Id: Ie50c14fed0e573dca0d98d5301240a691375a99e

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8484,6 +8484,13 @@ public class PackageManagerService extends IPackageManager.Stub
         }
     }
 
+    private String[] systemOverlayPackages = {"SysuiDarkTheme",
+                                              "DisplayCutoutEmulationCorner",
+                                              "DisplayCutoutEmulationDouble",
+                                              "DisplayCutoutEmulationNarrow",
+                                              "DisplayCutoutEmulationTall",
+                                              "DisplayCutoutEmulationWide"};
+
     private void scanDirLI(File scanDir, int parseFlags, int scanFlags, long currentTime) {
         final File[] files = scanDir.listFiles();
         if (ArrayUtils.isEmpty(files)) {
@@ -8505,6 +8512,12 @@ public class PackageManagerService extends IPackageManager.Stub
                         && !PackageInstallerService.isStageName(file.getName());
                 if (!isPackage) {
                     // Ignore entries which are not packages
+                    continue;
+                }
+                // Ignore vendor overlays that should live on system/app
+                if ((scanDir.getPath() == VENDOR_OVERLAY_DIR || scanDir.getPath() == PRODUCT_OVERLAY_DIR)
+                        && Arrays.asList(systemOverlayPackages).contains(file.getName())){
+                    Slog.w(TAG, "Ignoring " + file.getAbsolutePath() + " because is already installed on /system/app/");
                     continue;
                 }
                 parallelPackageParser.submit(file, parseFlags);
