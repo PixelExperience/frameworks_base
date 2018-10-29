c328287c8fe6f9a68f83eadbb275c59e1a22ed73
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-29 17:32:21 -0300

    SystemUI: Implement weather on lockscreen like Pixels
    This use OmniJaws implementations as base, very thanks to Omni team
    
    Icons extracted from Lawnchair, thanks to deletescape and paphonb
    
    WeatherClient is included on vendor/pixelstyle
    
    Our edits:
    
    * Cleanup code
    * Adapt to our weather provider
    * Slice-ify
    * Add required permissions
    
    Change-Id: I05c3d109edb9c38ad11e14dfc17f4dd06a9bcb1d
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -80,6 +80,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.android.internal.util.custom.weather.WeatherClient;
+
 /**
  * This class is the policy for granting runtime permissions to
  * platform components and default handlers in the system such
@@ -854,6 +856,12 @@ public final class DefaultPermissionGrantPolicy {
         if (conpro2Package != null) {
             grantRuntimePermissions(conpro2Package, CONTACTS_PERMISSIONS, true, userId);
             grantRuntimePermissions(conpro2Package, STORAGE_PERMISSIONS, true, userId);
+        }
+
+        // Weather client
+        PackageParser.Package weatherClientPackage = getSystemPackage(WeatherClient.SERVICE_PACKAGE);
+        if (weatherClientPackage != null && doesPackageSupportRuntimePermissions(weatherClientPackage)) {
+            grantRuntimePermissions(weatherClientPackage, LOCATION_PERMISSIONS, userId);
         }
 
         if (mPermissionGrantedCallback != null) {
