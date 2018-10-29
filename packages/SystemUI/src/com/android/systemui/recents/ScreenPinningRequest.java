7f0cd70a4389249ca4b20361eabdddb6ceb12aaa
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-28 21:54:21 -0300

    Fix screen unpinning text on devices that have gestures on fp sensor
    * Also fix hasNavigationBar logic
    
    Change-Id: I0666acb0e8d87224c95ffeea78e07cfc0f05d3ae

--- packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
+++ packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
@@ -55,6 +55,8 @@ import java.util.ArrayList;
 import static com.android.systemui.util.leak.RotationUtils.ROTATION_LANDSCAPE;
 import static com.android.systemui.util.leak.RotationUtils.ROTATION_SEASCAPE;
 
+import com.android.internal.util.custom.NavbarUtils;
+
 public class ScreenPinningRequest implements View.OnClickListener {
 
     private final Context mContext;
@@ -253,7 +255,7 @@ public class ScreenPinningRequest implements View.OnClickListener {
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(INVISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(INVISIBLE);
                 descriptionStringResId = !hasNavigationBar()
-                        ? R.string.screen_pinning_description_no_navbar
+                        ? (supportsGesturesOnFP() ? R.string.screen_pinning_description_no_navbar_fpsensor : R.string.screen_pinning_description_no_navbar)
                         : touchExplorationEnabled
                         ? R.string.screen_pinning_description_accessible
                         : R.string.screen_pinning_description;
@@ -262,7 +264,7 @@ public class ScreenPinningRequest implements View.OnClickListener {
                 mLayout.findViewById(R.id.screen_pinning_home_bg_light).setVisibility(VISIBLE);
                 mLayout.findViewById(R.id.screen_pinning_home_bg).setVisibility(VISIBLE);
                 descriptionStringResId = !hasNavigationBar()
-                        ? R.string.screen_pinning_description_no_navbar
+                        ? (supportsGesturesOnFP() ? R.string.screen_pinning_description_no_navbar_fpsensor : R.string.screen_pinning_description_no_navbar)
                         : touchExplorationEnabled
                         ? R.string.screen_pinning_description_recents_invisible_accessible
                         : R.string.screen_pinning_description_recents_invisible;
@@ -316,11 +318,15 @@ public class ScreenPinningRequest implements View.OnClickListener {
 
         private boolean hasNavigationBar() {
             try {
-                return mWindowManagerService.hasNavigationBar();
+                return mWindowManagerService.hasNavigationBar() && NavbarUtils.isEnabled(mContext);
             } catch (RemoteException e) {
                 // ignore
             }
             return false;
+        }
+
+        private boolean supportsGesturesOnFP() {
+            return mContext.getResources().getBoolean(com.android.internal.R.bool.config_supportsGesturesOnFingerprintSensor);
         }
 
         @Override
