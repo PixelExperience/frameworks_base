e181151773ad29c4d965b34cb2a5d9ef4c4ee2cc
Alex Cruz <alex@dirtyunicorns.com>
2018-11-08 14:58:57 -0600

    Themes: Fix issue applying themes with certain wallpapers / LWP
    If you applied a wallpaper that was mostly black and then attempted to
    switch the theme to light, it wouldn't let you. You would of had to switch
    the wallpaper and then the theme. This is just not ideal for the average
    user or anyone really. This fixes that fail.
    
    Steps to reproduce:
    - Set default theme to light
    - Download/Install Google wallpapers
    - Apply the 'moon' wallpaper
    - Attempt to switch theme to dark
    
    Change-Id: I7ca4ff2b49377434bb1c27dda3b505c8574607cd

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4038,7 +4038,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         final boolean nightModeWantsDarkTheme = DARK_THEME_IN_NIGHT_MODE
                 && (config.uiMode & Configuration.UI_MODE_NIGHT_MASK)
                     == Configuration.UI_MODE_NIGHT_YES;
-        final boolean useDarkTheme = wallpaperWantsDarkTheme || nightModeWantsDarkTheme;
+        final boolean useDarkTheme = nightModeWantsDarkTheme;
         if (isUsingDarkTheme() != useDarkTheme) {
             mUiOffloadThread.submit(() -> {
                 try {
