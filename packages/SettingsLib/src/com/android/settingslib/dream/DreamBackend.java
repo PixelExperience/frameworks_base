9deb23cc0f0fc63cae4ee36ca4b9f6796fdbc0eb
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-11-08 22:14:35 -0200

    DreamBackend: Fix FC on screensaver settings
    Change-Id: I0c8b0d33fcb0cb3b4e5a01a947149f47220535a3

--- packages/SettingsLib/src/com/android/settingslib/dream/DreamBackend.java
+++ packages/SettingsLib/src/com/android/settingslib/dream/DreamBackend.java
@@ -260,7 +260,10 @@ public class DreamBackend {
         logd("launchSettings(%s)", dreamInfo);
         if (dreamInfo == null || dreamInfo.settingsComponentName == null)
             return;
-        mContext.startActivity(new Intent().setComponent(dreamInfo.settingsComponentName));
+        Intent dreamIntent = new Intent();
+        dreamIntent.setComponent(dreamInfo.settingsComponentName);
+        dreamIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(dreamIntent);
     }
 
     public void preview(DreamInfo dreamInfo) {
