bb5b444a9806e4173e449e0983641dadce3399f4
SpiritCroc <dev@spiritcroc.de>
2018-11-11 13:01:56 +0000

    Add network traffic monitor properly to statusbar icons
    Make it behave fine on devices that don't have much status bar space
    (e.g. because of a display cutout/"notch")
    
    Includes also:
    commit 91a524cc7aea76225a4e3820fe19a78b7b31e95c
    Author: SpiritCroc <dev@spiritcroc.de>
    Date:   Tue Nov 20 14:42:48 2018 +0100
    
        Avoid casting network traffic to StatusBarIconView to prevent crash
    
        Change-Id: Ib7dab59daec436f91a7c9d213583feb52f3d2b21
    
    @jhenrique09 edits: Adapt to PE, cleanup lineage implementation
    
    Change-Id: I9e607521013f2aaedb9c4eb0e0b1d9743ebc8ab9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -62,7 +62,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private LinearLayout mSystemIconArea;
     private View mClockView;
     private View mNotificationIconAreaInner;
-    private View mNetworkTrafficHolder;
     private int mDisabled1;
     private StatusBar mStatusBarComponent;
     private DarkIconManager mDarkIconManager;
@@ -106,7 +105,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         Dependency.get(StatusBarIconController.class).addIconGroup(mDarkIconManager);
         mSystemIconArea = mStatusBar.findViewById(R.id.system_icon_area);
         mClockView = mStatusBar.findViewById(R.id.clock);
-        mNetworkTrafficHolder = mStatusBar.findViewById(R.id.network_traffic_holder);
         showSystemIconArea(false);
         showClock(false);
         initEmergencyCryptkeeperText();
@@ -237,12 +235,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     public void hideSystemIconArea(boolean animate) {
         animateHide(mSystemIconArea, animate);
-        animateHide(mNetworkTrafficHolder, animate);
     }
 
     public void showSystemIconArea(boolean animate) {
         animateShow(mSystemIconArea, animate);
-        animateShow(mNetworkTrafficHolder, animate);
     }
 
     public void hideClock(boolean animate) {
