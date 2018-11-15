29b29f6fed72f439394d76f70ef43c6a2d7ae2eb
dianlujitao <dianlujitao@lineageos.org>
2018-11-15 11:33:20 +0800

    TunerServiceImpl: Blacklist Settings.System from tuner reset
     * They use the tunable infrastructure but are real user settings and
       shouldn't be reset with tuner settings. Moreover, setting them to
       null would cause a IllegalArgumentException.
    
    @jhenrique09 edits: Adapted to PE
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I8d8b35fb4a8699480c158610e82f67c0fba622e1

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -265,7 +265,7 @@ public class TunerServiceImpl extends TunerService {
         mContext.sendBroadcast(intent);
 
         for (String key : mTunableLookup.keySet()) {
-            if (ArrayUtils.contains(RESET_BLACKLIST, key)) {
+            if (ArrayUtils.contains(RESET_BLACKLIST, key) || key.startsWith("system:")) {
                 continue;
             }
             Settings.Secure.putString(mContentResolver, key, null);
