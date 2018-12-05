efdedca9c0ac8390ad37771469fe1f9e2a520884
ezio84 <brabus84@gmail.com>
2018-12-05 08:29:58 +0100

    Led lights: don't skip dialer and other ones
    https://github.com/ezio84/abc_frameworks_base/commit/543692372b0bcfba1f04bd650cfd69a3df8ceb34#diff-d7b00843a7d21e20fe1ee38e433c02c6
    made the check just too restrictive, it doesn't work well
    with a custom lights feature like the one we have.
    
    Change-Id: Ia0f9f3468bc631086845ff59a0543900a4ce98de

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4848,7 +4848,7 @@ public class NotificationManagerService extends SystemService {
         }
         if (buzz || beep || blink) {
             // Ignore summary updates because we don't display most of the information.
-            if (record.sbn.isGroup() && record.sbn.getNotification().isGroupSummary()) {
+            /*if (record.sbn.isGroup() && record.sbn.getNotification().isGroupSummary()) {
                 if (DEBUG_INTERRUPTIVENESS) {
                     Log.v(TAG, "INTERRUPTIVENESS: "
                             + record.getKey() + " is not interruptive: summary");
@@ -4857,9 +4857,9 @@ public class NotificationManagerService extends SystemService {
                 if (DEBUG_INTERRUPTIVENESS) {
                     Log.v(TAG, "INTERRUPTIVENESS: "
                             + record.getKey() + " is interruptive: alerted");
-                }
+                }*/
                 record.setInterruptive(true);
-            }
+            //}
             MetricsLogger.action(record.getLogMaker()
                     .setCategory(MetricsEvent.NOTIFICATION_ALERT)
                     .setType(MetricsEvent.TYPE_OPEN)
@@ -4891,7 +4891,7 @@ public class NotificationManagerService extends SystemService {
             return false;
         }
         // Suppressed because it's a silent update
-        final Notification notification = record.getNotification();
+        /*final Notification notification = record.getNotification();
         if (record.isUpdate && (notification.flags & Notification.FLAG_ONLY_ALERT_ONCE) != 0) {
             return false;
         }
@@ -4902,7 +4902,7 @@ public class NotificationManagerService extends SystemService {
         // not if in call or the screen's on
         if (mInCall || mScreenOn) {
             return false;
-        }
+        }*/
 
         return true;
     }
