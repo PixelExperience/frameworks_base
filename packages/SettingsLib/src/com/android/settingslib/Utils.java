2617c48d3e643b84f7e2a4a9b3e2573e3e882b3c
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-01-03 16:47:22 +0100

    Display 'Dash charging' for dash charge in battery settings [1/2]
    Change-Id: Ia7a91062a88b766840fb584af5c5821dcf206e47

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -177,9 +177,13 @@ public class Utils {
     public static String getBatteryStatus(Resources res, Intent batteryChangedIntent) {
         int status = batteryChangedIntent.getIntExtra(BatteryManager.EXTRA_STATUS,
                 BatteryManager.BATTERY_STATUS_UNKNOWN);
+        boolean dashChargeStatus = batteryChangedIntent.getBooleanExtra(
+                BatteryManager.EXTRA_DASH_CHARGER, false);
         String statusString;
         if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
-            statusString = res.getString(R.string.battery_info_status_charging);
+            statusString = dashChargeStatus
+                    ? res.getString(R.string.battery_info_status_dash_charging)
+                    : res.getString(R.string.battery_info_status_charging);
         } else if (status == BatteryManager.BATTERY_STATUS_DISCHARGING) {
             statusString = res.getString(R.string.battery_info_status_discharging);
         } else if (status == BatteryManager.BATTERY_STATUS_NOT_CHARGING) {
