96bcfd1282934c09cae4f63c85a9397fd9b74e04
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 10:59:49 -0600

    Bring back traffic indicator for all!
    arrow-edits:
     - adapt to Q
    
    Change-Id: Ia41474a262b258f8a2d4d7b10a15891729ffd17c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4845,6 +4845,18 @@ public final class Settings {
                 ANY_STRING_VALIDATOR;
 
         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
