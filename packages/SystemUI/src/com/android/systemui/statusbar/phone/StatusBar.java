9c5e2ca21bb2e8de3ca5c5108a50e460ff22d5e1
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-01-18 03:22:49 -0200

    SystemUI: Fix flickering issue when live wallpaper is showing on aod/pulsing
    Steps to reproduce:
    
    Install Pixel 3 live wallpapers and use one by default > Lock device > Trigger ambient display > Unlock with fp
    
    Change-Id: I2a6af7a37e75ef1984aaf0647a5d25b2d16e8180

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -519,6 +519,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected NotificationLockscreenUserManager mLockscreenUserManager;
     protected NotificationRemoteInputManager mRemoteInputManager;
 
+    private boolean mWallpaperSupportsAmbientMode;
     private BroadcastReceiver mWallpaperChangedReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -528,13 +529,16 @@ public class StatusBar extends SystemUI implements DemoMode,
                 return;
             }
             WallpaperInfo info = wallpaperManager.getWallpaperInfo();
-            final boolean supportsAmbientMode = info != null &&
-                    info.getSupportsAmbientMode();
+            mWallpaperSupportsAmbientMode = info != null && info.getSupportsAmbientMode();
 
-            mStatusBarWindowManager.setWallpaperSupportsAmbientMode(supportsAmbientMode);
-            mScrimController.setWallpaperSupportsAmbientMode(supportsAmbientMode);
+            mStatusBarWindowManager.setWallpaperSupportsAmbientMode(mWallpaperSupportsAmbientMode);
+            mScrimController.setWallpaperSupportsAmbientMode(mWallpaperSupportsAmbientMode);
         }
     };
+
+    public boolean wallpaperSupportsAmbientMode(){
+        return mWallpaperSupportsAmbientMode;
+    }
 
     private Runnable mLaunchTransitionEndRunnable;
     protected boolean mLaunchTransitionFadingAway;
