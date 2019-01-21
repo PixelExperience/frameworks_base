31284effcc1dcf9204e451dd84df6ed8c965900f
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-01-21 20:38:44 -0200

    fwb: Fix animation issue when opening Messaging app
    Change-Id: Ibf95d09fe3b5d569a68cb93cb6a0c1d058d7db56

--- services/core/java/com/android/server/UiModeManagerService.java
+++ services/core/java/com/android/server/UiModeManagerService.java
@@ -293,6 +293,7 @@ final class UiModeManagerService extends SystemService {
             }
         }
 
+        private int messagingAppNightModeState = -1;
         @Override
         public void setNightMode(int mode) {
             try{
@@ -300,13 +301,18 @@ final class UiModeManagerService extends SystemService {
                 List<ActivityManager.RunningTaskInfo> runningTasks = manager.getRunningTasks(1);
                 if (runningTasks != null && runningTasks.size() > 0){
                     String packageName = runningTasks.get(0).topActivity.getPackageName();
-                    if (packageName.equals("com.google.android.apps.messaging")) {
-                        Intent launchIntent = getContext().getPackageManager().getLaunchIntentForPackage("com.google.android.apps.messaging");
-                        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    if (packageName.equals("com.google.android.apps.messaging") && messagingAppNightModeState != mode) {
+                        if (messagingAppNightModeState == -1){ // System booted, store the desired value and don't restart app to prevent flickering
+                            messagingAppNightModeState = mode;
+                            return;
+                        }
+                        messagingAppNightModeState = mode;
+                        Intent launchIntent = getContext().getPackageManager().getLaunchIntentForPackage(packageName);
+                        launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         getContext().startActivity(launchIntent);
                         return;
-                    }
-                }
+                   }
+               }
             }catch(Exception e){
             }
             if (isNightModeLocked() &&  (getContext().checkCallingOrSelfPermission(
