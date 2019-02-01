4edc533824174cb705117a0bd354cda2ff1b07e5
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-01 13:38:12 -0200

    Theme Wellbeing
    Change-Id: Ie7b8cd9c6f07aff1ecec97d71eb97b696da021b2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4049,6 +4049,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                             useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                     mOverlayManager.setEnabled("com.android.gboard.theme.light",
                             !useDarkTheme, mLockscreenUserManager.getCurrentUserId());
+                    mOverlayManager.setEnabled("com.android.wellbeing.theme.dark",
+                            useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                     if (useDarkTheme) {
                         unloadStockDarkTheme();
                     }
