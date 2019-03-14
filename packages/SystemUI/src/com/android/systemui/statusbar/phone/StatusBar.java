46e5a1a37d954c91e9ca4ad815231415bf81e660
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-14 00:13:51 -0300

    Smart clock improvements
    * Disable on decrypt screen
    * Disable when notch is hidden
    * Disable when at least one icon are showing, add interfaces
    
    Change-Id: Ifa6f32ae37159cfd510d5e40f45d738d4a023c6c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -352,6 +352,8 @@ public class StatusBar extends SystemUI implements DemoMode,
      */
     private static final float SRC_MIN_ALPHA = 0.002f;
 
+    private CollapsedStatusBarFragment statusBarFragment;
+
     static {
         boolean onlyCoreApps;
         try {
@@ -884,9 +886,14 @@ public class StatusBar extends SystemUI implements DemoMode,
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mNotificationIconAreaController);
         FragmentHostManager.get(mStatusBarWindow)
                 .addTagListener(CollapsedStatusBarFragment.TAG, (tag, fragment) -> {
-                    CollapsedStatusBarFragment statusBarFragment =
-                            (CollapsedStatusBarFragment) fragment;
+                    statusBarFragment = (CollapsedStatusBarFragment) fragment;
                     statusBarFragment.initNotificationIconArea(mNotificationIconAreaController);
+                    mNotificationIconAreaController.setNotificationIconsCountListener(new NotificationIconAreaController.NotificationIconsCountListener(){
+                        @Override
+                        public void onCountChanged(int count){
+                            statusBarFragment.onNotificationsIconsCountChanged(count);
+                        }
+                    });
                     PhoneStatusBarView oldStatusBarView = mStatusBarView;
                     mStatusBarView = (PhoneStatusBarView) fragment.getView();
                     mStatusBarView.setBar(this);
@@ -3365,6 +3372,9 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         mViewHierarchyManager.updateRowStates();
         mScreenPinningRequest.onConfigurationChanged();
+        if (statusBarFragment != null){
+            statusBarFragment.setLandscape(newConfig.orientation != Configuration.ORIENTATION_PORTRAIT);
+        }
     }
 
     @Override
