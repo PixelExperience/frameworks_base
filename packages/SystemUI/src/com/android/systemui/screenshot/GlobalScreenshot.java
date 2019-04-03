LineageOS/android_frameworks_base

Clean up screenshot if nothing selected
If rect is null, run the finisher so the screenshot logic
is cleaned up and prepared for next screenshot. This fixes
BUGBASH-1831.

[basilgello: combined two if-clauses to avoid double else,
  inspired by @BadDaemon]
Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>

Change-Id: Ib46221df2a2a70bc5a901107ca6aa4268a0568f2

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -642,16 +642,18 @@ class GlobalScreenshot {
                         view.setVisibility(View.GONE);
                         mWindowManager.removeView(mScreenshotSelectorLayout);
                         final Rect rect = view.getSelectionRect();
-                        if (rect != null) {
-                            if (rect.width() != 0 && rect.height() != 0) {
-                                // Need mScreenshotSelectorLayout to handle it after the view disappears
-                                mScreenshotSelectorLayout.post(new Runnable() {
-                                    public void run() {
-                                        takeScreenshot(finisher, statusBarVisible, navBarVisible,
-                                                rect.left, rect.top, rect.width(), rect.height());
-                                    }
-                                });
-                            }
+                        if (rect != null && rect.width() != 0 && rect.height() != 0) {
+                            // Need mScreenshotSelectorLayout to handle it after the view disappears
+                            mScreenshotSelectorLayout.post(new Runnable() {
+                                public void run() {
+                                    takeScreenshot(finisher, statusBarVisible, navBarVisible,
+                                            rect.left, rect.top, rect.width(), rect.height());
+                                }
+                            });
+                        } else {
+                            // Clean up if nothing is selected
+                            // to initialize a new screenshot later
+                            finisher.run();
                         }
 
                         view.stopSelection();

PixelExperience/frameworks_base

  inspired by BadDaemon]

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -626,16 +626,18 @@ class GlobalScreenshot {
                         view.setVisibility(View.GONE);
                         mWindowManager.removeView(mScreenshotLayout);
                         final Rect rect = view.getSelectionRect();
-                        if (rect != null) {
-                            if (rect.width() != 0 && rect.height() != 0) {
-                                // Need mScreenshotLayout to handle it after the view disappears
-                                mScreenshotLayout.post(new Runnable() {
-                                    public void run() {
-                                        takeScreenshot(finisher, statusBarVisible, navBarVisible,
-                                                rect);
-                                    }
-                                });
-                            }
+                        if (rect != null && rect.width() != 0 && rect.height() != 0) {
+                            // Need mScreenshotLayout to handle it after the view disappears
+                            mScreenshotLayout.post(new Runnable() {
+                                public void run() {
+                                    takeScreenshot(finisher, statusBarVisible, navBarVisible,
+                                            rect);
+                                }
+                            });
+                        } else {
+                            // Clean up if nothing is selected
+                            // to initialize a new screenshot later
+                            finisher.run();
                         }
 
                         view.stopSelection();