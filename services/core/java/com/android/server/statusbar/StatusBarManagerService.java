9e305c9d733807561e1c74e7b216013b0ffbc6d3
Peter Cai <peter@typeblog.net>
2019-04-05 16:44:18 +0200

    Initial support for in-display fingerprint sensors
    Change-Id: I52beaf6492b9daa012b5d42b858d552d1e378a2c

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -597,6 +597,17 @@ public class StatusBarManagerService extends IStatusBarService.Stub {
     }
 
     @Override
+    public void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) {
+        if (mBar != null) {
+            try {
+                mBar.handleInDisplayFingerprintView(show, isEnrolling);
+            } catch (RemoteException ex) {
+                // do nothing
+            }
+        }
+    }
+
+    @Override
     public void toggleCameraFlash() {
         if (mBar != null) {
             try {
