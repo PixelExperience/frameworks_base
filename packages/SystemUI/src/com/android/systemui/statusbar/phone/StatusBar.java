1dfb9f13f65040e17477a24465c05a3a4a46767b
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-30 10:44:50 -0300

    fwb: Changes for custom accents
    * Statusbar: Swap black/white accent color when theme changes
    
    Change-Id: I0dff8f0f3fcbce42f2ea89b2730c791741dbc518

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2263,6 +2263,33 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         return themeInfo != null && themeInfo.isEnabled();
     }
 
+    private void swapWhiteBlackAccent() {
+        OverlayInfo themeInfo = null;
+        try {
+            if (isUsingDarkTheme()){
+                themeInfo = mOverlayManager.getOverlayInfo("org.pixelexperience.overlay.accent.black",
+                        mLockscreenUserManager.getCurrentUserId());
+                if (themeInfo != null && themeInfo.isEnabled()) {
+                    mOverlayManager.setEnabled("org.pixelexperience.overlay.accent.black",
+                            false, mLockscreenUserManager.getCurrentUserId());
+                    mOverlayManager.setEnabled("org.pixelexperience.overlay.accent.white",
+                            true, mLockscreenUserManager.getCurrentUserId());
+                }
+            } else {
+                themeInfo = mOverlayManager.getOverlayInfo("org.pixelexperience.overlay.accent.white",
+                        mLockscreenUserManager.getCurrentUserId());
+                if (themeInfo != null && themeInfo.isEnabled()) {
+                    mOverlayManager.setEnabled("org.pixelexperience.overlay.accent.white",
+                            false, mLockscreenUserManager.getCurrentUserId());
+                    mOverlayManager.setEnabled("org.pixelexperience.overlay.accent.black",
+                            true, mLockscreenUserManager.getCurrentUserId());
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void unloadStockDarkTheme() {
         OverlayInfo themeInfo = null;
         try {
@@ -4147,6 +4174,9 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 } catch (RemoteException e) {
                     Log.w(TAG, "Can't change theme", e);
                 }
+            });
+            mUiOffloadThread.submit(() -> {
+                swapWhiteBlackAccent();
             });
         }
 
