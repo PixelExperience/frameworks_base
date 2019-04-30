a1a988eda343ab8073cb972e3707f76386494014
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-30 12:45:29 -0300

    fwb: Hwkeys disable - rework
    Logic based on keydisabler
    
    Change-Id: I37e17181b9eeebbe9da5989fd0725aa36e10cb39

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2691,7 +2691,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mNavigationBarCanMove = width != height && shortSizeDp < 600;
 
         mHasNavigationBar = NavbarUtils.isEnabled(mContext);
-        updateKeydisabler();
 
         // For demo purposes, allow the rotation of the HDMI display to be controlled.
         // By default, HDMI locks rotation to landscape.
@@ -2859,7 +2858,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             mHasNavigationBar = NavbarUtils.isEnabled(mContext);
-            updateKeydisabler();
         }
         synchronized (mWindowManagerFuncs.getWindowManagerLock()) {
             PolicyControl.reloadFromSetting(mContext);
@@ -2867,10 +2865,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (updateRotation) {
             updateRotation(true);
         }
-    }
-
-    private void updateKeydisabler(){
-        // TODO: Add hwkeys code here
     }
 
     private void updateWakeGestureListenerLp() {
@@ -3903,6 +3897,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
         };
 
+    private boolean shouldDisableKey(int keyCode) {
+        return mHasNavigationBar && (keyCode == KeyEvent.KEYCODE_HOME
+                || keyCode == KeyEvent.KEYCODE_MENU
+                || keyCode == KeyEvent.KEYCODE_APP_SWITCH
+                || keyCode == KeyEvent.KEYCODE_ASSIST
+                || keyCode == KeyEvent.KEYCODE_BACK);
+    }
+
     /** {@inheritDoc} */
     @Override
     public long interceptKeyBeforeDispatching(WindowState win, KeyEvent event, int policyFlags) {
@@ -3921,6 +3923,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             Log.d(TAG, "interceptKeyTi keyCode=" + keyCode + " down=" + down + " repeatCount="
                     + repeatCount + " keyguardOn=" + keyguardOn + " mHomePressed=" + mHomePressed
                     + " canceled=" + canceled);
+        }
+
+        // Hardware keys disable
+        if (!fromNavbar && shouldDisableKey(keyCode)) {
+            return -1;
         }
 
         // If we think we might have a volume down & power key chord on the way
@@ -6585,6 +6592,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && (!isNavBarVirtKey || mNavBarVirtualKeyHapticFeedbackEnabled)
                 && event.getRepeatCount() == 0;
+
+        if (!fromNavbar && shouldDisableKey(keyCode)){
+            useHapticFeedback = false;
+        }
 
         // Specific device key handling
         if (dispatchKeyToKeyHandlers(event)) {
