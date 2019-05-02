e48de4c2a6e656e7f631da17cf2ec5562f9d2001
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-02 11:20:42 -0300

    Add VOLTE icon and switch on tuner (CAF edition)
    Squashed commits:
    
    commit 1c5a1000e78b47be3e463d0a32253ed81d9fec3d
    Author: Adarsh-MR <adarshmr1998@gmail.com>
    Date:   Fri Oct 19 11:51:38 2018 +0000
    
        Fix VOLTE icon color on Light statusbar
    
        Change-Id: I96803c61311e8fafe8a395c0afe03c9a33d07feb
        Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    commit c605980a4f6b2ef4074f6ae5e9cdd0172917b1f7
    Author: Anushek <anushekprasal@gmail.com>
    Date:   Mon Jun 4 13:24:53 2018 +0530
    
        SystemUI: Use VoLTE icon from OOS
    
    Change-Id: I4b7752e32d8b08a47647016584aa7dc4a20706bc
    
    commit 928892fe251d5b715701b5a04fb1035375e2d4c5
    Author: SagarMakhar <sagarmakhar@gmail.com>
    Date:   Mon Oct 22 18:50:29 2018 +0000
    
        Add VOLTE icon toggle [1/2]
    
        Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    @jhenrique09 edits: Add switch on tuner, notch handling and cleanup code for caf
    
    Change-Id: I05bcf204a28675a7c7d6720be81c215d9f100b76

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -237,6 +237,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         mMobileRoaming.setImageTintList(color);
         mDotView.setDecorColor(tint);
         mDotView.setIconColor(tint, false);
+        mVolte.setImageTintList(color);
     }
 
     @Override
@@ -258,6 +259,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         mOut.setImageTintList(list);
         mMobileType.setImageTintList(list);
         mMobileRoaming.setImageTintList(list);
+        mVolte.setImageTintList(list);
         mDotView.setDecorColor(color);
     }
 
