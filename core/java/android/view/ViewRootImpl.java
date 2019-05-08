490d5ba0ff22918567289bbb9030f0a838645c1d
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-08 11:52:38 -0300

    fwb: Import SwipeToScreenshot implementation from MIUI
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    commit 827837b0e2deb21587ac24be99ac98e5828fb400 (HEAD -> ten-plus)
    Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Date:   Wed Oct 9 15:07:19 2019 +0000
    
        PhoneWindowManager: Disable three finger to screenshot on doze
    
        Change-Id: Ic7b38ffbe1980e78f9573dc2b0dc0b850d2c3ec2
    
    Change-Id: Iffdff674ba3419126df22150b47d1cb765b000d7

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -6752,6 +6752,10 @@ public final class ViewRootImpl implements ViewParent,
             final MotionEvent event = (MotionEvent)q.mEvent;
             mHandwritingInitiator.onTouchEvent(event);
 
+            if (event.getPointerCount() == 3 && isSwipeToScreenshotGestureActive()) {
+                event.setAction(MotionEvent.ACTION_CANCEL);
+            }
+
             mAttachInfo.mUnbufferedDispatchRequested = false;
             mAttachInfo.mHandlingPointerEvent = true;
             boolean handled = mView.dispatchPointerEvent(event);
@@ -11227,5 +11231,13 @@ public final class ViewRootImpl implements ViewParent,
             return;
         }
         mSurfaceSyncer.merge(mSyncId, syncId, otherSyncer);
+    }
+
+    private boolean isSwipeToScreenshotGestureActive() {
+        try {
+            return ActivityManager.getService().isSwipeToScreenshotGestureActive();
+        } catch (RemoteException e) {
+            return false;
+        }
     }
 }
