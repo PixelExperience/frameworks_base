4a024527bfdcb51902d26c9057186068f4d7b64c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-13 16:19:34 -0300

    fwb: Hwkeys disable: Improve disabling on some cases
    Change-Id: I0d02b4be84d3bd6555cba946bb006973b34510c2
    (cherry picked from commit 37b16baf39f9ef4ecd0d095b4a3d6ef46893c15d)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6896,6 +6896,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     + " policyFlags=" + Integer.toHexString(policyFlags));
         }
 
+        if (!fromNavbar && !virtualKey && shouldDisableKey(keyCode)){
+            return 0;
+        }
+
         // Pre-basic policy based on interactive and pocket lock state.
         if (mIsDeviceInPocket && (!interactive || mPocketLockShowing)) {
             if (keyCode != KeyEvent.KEYCODE_POWER &&
@@ -6971,10 +6975,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && (!isNavBarVirtKey || mNavBarVirtualKeyHapticFeedbackEnabled)
                 && event.getRepeatCount() == 0;
-
-        if (!fromNavbar && !virtualKey && shouldDisableKey(keyCode)){
-            useHapticFeedback = false;
-        }
 
         // Specific device key handling
         if (dispatchKeyToKeyHandlers(event)) {
