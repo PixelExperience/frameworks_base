787d9125ef5a32189f2f034df0210e4e22cb4087
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-05 19:28:25 -0300

    Move overlays packages names to own class
    Change-Id: I735416aee0794eb9db4d277ed7fb577488a8e3b7

---
+++ core/java/com/android/internal/util/custom/OverlayUtils.java
@@ -0,0 +1,46 @@
+/*
+* Copyright (C) 2017 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.custom;
+
+public class OverlayUtils {
+    public static String[] AllPackages = {"com.android.documentsui.theme.dark",
+            "com.android.systemui.custom.theme.dark",
+            "com.android.gboard.theme.dark",
+            "com.android.settings.intelligence.theme.dark",
+            "com.android.wellbeing.theme",
+            "com.android.system.theme.dark",
+            "com.android.settings.theme.dark",
+            "org.pixelexperience.ota.theme.dark",
+            "com.android.documentsui.theme.black",
+            "com.android.systemui.custom.theme.black",
+            "com.android.system.theme.black",
+            "org.pixelexperience.ota.theme.black",
+            "org.pixelexperience.overlay.accent.cyan",
+            "org.pixelexperience.overlay.accent.purple",
+            "org.pixelexperience.overlay.accent.white",
+            "org.pixelexperience.overlay.accent.red",
+            "org.pixelexperience.overlay.accent.brown",
+            "org.pixelexperience.overlay.accent.yellow",
+            "org.pixelexperience.overlay.accent.teal",
+            "org.pixelexperience.overlay.accent.black",
+            "org.pixelexperience.overlay.accent.orange",
+            "org.pixelexperience.overlay.accent.green",
+            "org.pixelexperience.overlay.accent.pink",
+            "com.android.gboard.theme.light",
+            "org.pixelexperience.overlay.hidecutout",
+            "com.android.facelock.theme.dark",
+            "com.google.android.setupwizard.overlay"};
+}
