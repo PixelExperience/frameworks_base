e2a4928b9d789c23e194f9bdd01f708f40834631
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-05 21:55:18 -0300

    Revert "StatusBarSignalPolicy: Revert los implementation for sim card provisioning"
    This reverts commit 3f24c030cf8dc8762ca34a7bfc67152bcff309df.
    
    Change-Id: Iec4016280f138364155fa0b6ac8deba7e33e52b0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -105,14 +105,13 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         if (subId != null) {
             MobileIconState state = getState(subId[0]);
             if (state != null) {
-                state.mProvisioned = isProvisioned;
-                if (!isProvisioned) {
-                    state.visible = false;
+                state.provisioned = isProvisioned;
+                if (!state.provisioned) {
+                    // This hopefully gets rid of silly leftover padding
+                    // after disabling one of the SIMs.
+                    state.typeId = 0;
                 }
             }
-
-            mNetworkController.removeCallback(this);
-            mNetworkController.addCallback(this);
         }
     }
 
@@ -217,7 +216,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         // Visibility of the data type indicator changed
         boolean typeChanged = statusType != state.typeId && (statusType == 0 || state.typeId == 0);
 
-        state.visible = statusIcon.visible && !mBlockMobile && state.mProvisioned;
+        state.visible = statusIcon.visible && !mBlockMobile;
         state.strengthId = statusIcon.icon;
         state.typeId = statusType;
         state.contentDescription = statusIcon.contentDescription;
@@ -417,17 +416,17 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         public int typeId;
         public boolean roaming;
         public boolean needsLeadingPadding;
+        public boolean provisioned;
         public String typeContentDescription;
-        private boolean mProvisioned = true;
         public Context context;
 
         private MobileIconState(int subId, Context context) {
             super();
             this.subId = subId;
+            this.context = context;
+
             TelephonyExtUtils extTelephony = TelephonyExtUtils.getInstance(context);
-            if (extTelephony.hasService()) {
-                mProvisioned = extTelephony.isSubProvisioned(subId);
-            }
+            this.provisioned = !extTelephony.hasService() || extTelephony.isSubProvisioned(subId);
         }
 
         @Override
@@ -445,6 +444,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
                     typeId == that.typeId &&
                     roaming == that.roaming &&
                     needsLeadingPadding == that.needsLeadingPadding &&
+                    provisioned == that.provisioned &&
                     Objects.equals(typeContentDescription, that.typeContentDescription);
         }
 
@@ -470,6 +470,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
             other.typeId = typeId;
             other.roaming = roaming;
             other.needsLeadingPadding = needsLeadingPadding;
+            other.provisioned = provisioned;
             other.typeContentDescription = typeContentDescription;
         }
 
