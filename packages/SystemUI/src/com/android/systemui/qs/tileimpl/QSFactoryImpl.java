dd8a8e92a759ab1aa83ded6913719e6fe6a3c391
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-06 17:48:02 -0300

    Revert "SystemUI: Add LiveDisplay tile"
    This reverts commit ec021eea17d5d358d8f1143095950bd63e96ba54.
    
    Change-Id: I9d794b09aee9b0eb27251c9284b550b86430c072

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -37,7 +37,6 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
-import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
@@ -108,8 +107,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new CaffeineTile(mHost);
             case "heads_up":
                 return new HeadsUpTile(mHost);
-            case "livedisplay":
-                return new LiveDisplayTile(mHost);
             case "sync":
                 return new SyncTile(mHost);
         }
