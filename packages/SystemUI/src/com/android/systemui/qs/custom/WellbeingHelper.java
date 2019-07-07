9d2baf5050f3ada2c0938bb6f65524ba0ca6713b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-07 17:56:33 -0300

    SystemUI: Disable wellbeing grayscale tile if reading mode available
    Change-Id: I4cbd91ec3161661b92c5bbcbbf0f34e44d6ade08

---
+++ packages/SystemUI/src/com/android/systemui/qs/custom/WellbeingHelper.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.custom;
+
+import android.content.ComponentName;
+import android.content.Context;
+import com.android.systemui.qs.external.CustomTile;
+
+import com.android.internal.custom.hardware.LineageHardwareManager;
+
+public class WellbeingHelper {
+    private static final String mWellbeingPackage = "com.google.android.apps.wellbeing/com.google.android.apps.wellbeing.screen.ui.GrayscaleTileService";
+
+    public static boolean shouldAddTile(Context context, ComponentName componentName) {
+        if (componentName.flattenToString().equals(mWellbeingPackage)) {
+            try {
+                LineageHardwareManager hardwareManager = LineageHardwareManager.getInstance(context);
+                return !hardwareManager.isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+            } catch (Exception e) {
+            }
+        }
+        return true;
+    }
+
+    public static boolean shouldAddTile(Context context, String spec) {
+        if (spec.startsWith(CustomTile.PREFIX)){
+            return shouldAddTile(context, CustomTile.getComponentFromSpec(spec));
+        }
+        return true;
+    }
+}
