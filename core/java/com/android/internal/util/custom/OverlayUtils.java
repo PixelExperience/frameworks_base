a2335072b6fd2c0494310fc73dfa7e6957b00b36
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-21 10:32:43 -0300

    OverlayUtils: Update overlays
    Change-Id: I76d0625ba3c5ddbfc37d89fcf65e59bb9d8d6c12

--- core/java/com/android/internal/util/custom/OverlayUtils.java
+++ core/java/com/android/internal/util/custom/OverlayUtils.java
@@ -16,7 +16,30 @@
 package com.android.internal.util.custom;
 
 public class OverlayUtils {
-    public static String[] AllPackages = {"com.google.android.setupwizard.overlay"};
+    public static String[] AllPackages = {"com.android.documentsui.theme.dark",
+        "com.android.systemui.custom.theme.dark",
+        "com.android.settings.intelligence.theme.dark",
+        "com.android.system.theme.dark",
+        "com.android.settings.theme.dark",
+        "org.pixelexperience.ota.theme.dark",
+        "com.android.documentsui.theme.black",
+        "com.android.systemui.custom.theme.black",
+        "com.android.system.theme.black",
+        "org.pixelexperience.ota.theme.black",
+        "org.pixelexperience.overlay.accent.cyan",
+        "org.pixelexperience.overlay.accent.purple",
+        "org.pixelexperience.overlay.accent.white",
+        "org.pixelexperience.overlay.accent.red",
+        "org.pixelexperience.overlay.accent.brown",
+        "org.pixelexperience.overlay.accent.yellow",
+        "org.pixelexperience.overlay.accent.teal",
+        "org.pixelexperience.overlay.accent.black",
+        "org.pixelexperience.overlay.accent.orange",
+        "org.pixelexperience.overlay.accent.green",
+        "org.pixelexperience.overlay.accent.pink",
+        "org.pixelexperience.overlay.hidecutout",
+        "com.android.facelock.theme.dark",
+        "com.google.android.setupwizard.overlay"};
     public static String[] systemOverlayPackages = {"SysuiDarkTheme",
                                                     "DisplayCutoutEmulationCorner",
                                                     "DisplayCutoutEmulationDouble",
