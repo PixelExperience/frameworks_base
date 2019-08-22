23834237651b50558e3fa0a05a835b88a43f072d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-21 23:33:42 -0300

    fwb: Add support for internal audio recording
    Change-Id: I953c07d9a511b53ddb79f6a7ce5b47ca0c079acb

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -876,6 +876,7 @@ public class VolumeDialogImpl implements VolumeDialog {
                         (isRingStream && mState.disallowRinger) ||
                         (isSystemStream && mState.disallowSystem))
                 : false;
+        final boolean routedToSubmixAndEarphone = isMusicStream && mState.routedToSubmixAndEarphone;
 
         // update slider max
         final int max = ss.levelMax * 100;
@@ -894,12 +895,12 @@ public class VolumeDialogImpl implements VolumeDialog {
         mConfigurableTexts.add(row.header, ss.name);
 
         // update icon
-        final boolean iconEnabled = (mAutomute || ss.muteSupported) && !zenMuted;
+        final boolean iconEnabled = (mAutomute || ss.muteSupported) && !zenMuted && !routedToSubmixAndEarphone;
         row.icon.setEnabled(iconEnabled);
         row.icon.setAlpha(iconEnabled ? 1 : 0.5f);
         final int iconRes =
                 isRingVibrate ? R.drawable.ic_volume_ringer_vibrate
-                : isRingSilent || zenMuted ? row.iconMuteRes
+                : isRingSilent || zenMuted || routedToSubmixAndEarphone ? row.iconMuteRes
                 : ss.routedToBluetooth ?
                         (ss.muted ? R.drawable.ic_volume_media_bt_mute
                                 : R.drawable.ic_volume_media_bt)
@@ -953,15 +954,15 @@ public class VolumeDialogImpl implements VolumeDialog {
             row.icon.setContentDescription(getStreamLabelH(ss));
         }
 
-        // ensure tracking is disabled if zenMuted
-        if (zenMuted) {
+        // ensure tracking is disabled if zenMuted or routedToSubmixAndEarphone
+        if (zenMuted || routedToSubmixAndEarphone) {
             row.tracking = false;
         }
         enableVolumeRowViewsH(row, !zenMuted);
 
         // update slider
-        final boolean enableSlider = !zenMuted;
-        final int vlevel = row.ss.muted && (!isRingStream && !zenMuted) ? 0
+        final boolean enableSlider = !zenMuted && !routedToSubmixAndEarphone;
+        final int vlevel = routedToSubmixAndEarphone ? max : row.ss.muted && (!isRingStream && !zenMuted) ? 0
                 : row.ss.level;
         updateVolumeRowSliderH(row, enableSlider, vlevel);
     }
