45368a250514c0dbc1967c44d26e7fbbe6327058
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-26 13:51:18 -0300

    Revert "LiveDisplayTile: Remove tile if unavailable"
    This reverts commit edd5e98d0e1ede114e7d9a06b4727ab0e24aabde.
    
    Change-Id: I8aae43b437de4c730b0ae8ea96114ec8ffe2028c

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
@@ -116,9 +116,6 @@ public class LiveDisplayTile extends QSTileImpl<LiveDisplayState> {
             mOutdoorModeAvailable = mLiveDisplay.getConfig().hasFeature(MODE_OUTDOOR) &&
                     !mLiveDisplay.getConfig().hasFeature(FEATURE_MANAGED_OUTDOOR_MODE);
             mDayTemperature = mLiveDisplay.getDayColorTemperature();
-            if (mNightDisplayAvailable && !mOutdoorModeAvailable){
-                mHost.removeTile(getTileSpec());
-            }
             return true;
         }
         return false;
