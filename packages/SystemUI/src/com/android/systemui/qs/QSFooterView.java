dbbe7722c8e9f828b60879fd7b70cfafc5a2191e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 22:48:14 -0300

    SystemUI: Remove build version from qs footer
    Change-Id: I7bb8f1422765d9e09bd50b43fa1e9849fe006344

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
@@ -58,14 +58,14 @@ public class QSFooterView extends FrameLayout {
     @Nullable
     private OnClickListener mExpandClickListener;
 
-    private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
+    /*private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
             new Handler(mContext.getMainLooper())) {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             super.onChange(selfChange, uri);
             setBuildText();
         }
-    };
+    };*/
 
     public QSFooterView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -80,7 +80,7 @@ public class QSFooterView extends FrameLayout {
 
         updateResources();
         setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_YES);
-        setBuildText();
+        //setBuildText();
     }
 
     private void setBuildText() {
@@ -154,15 +154,15 @@ public class QSFooterView extends FrameLayout {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        mContext.getContentResolver().registerContentObserver(
+        /*mContext.getContentResolver().registerContentObserver(
                 Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false,
-                mDeveloperSettingsObserver, UserHandle.USER_ALL);
+                mDeveloperSettingsObserver, UserHandle.USER_ALL);*/
     }
 
     @Override
     @VisibleForTesting
     public void onDetachedFromWindow() {
-        mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
+        //mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
         super.onDetachedFromWindow();
     }
 
