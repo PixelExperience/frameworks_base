66b14a828ec5577c7f545a04ae4dbb9933d3664b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-21 20:10:58 +0000

    Fix smartspace on Pie
    Change-Id: I3220bc6a78f951024bca83ed095e9639a263492e

--- packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
+++ packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
@@ -2,11 +2,13 @@ package com.google.android.systemui;
 
 import android.app.AlarmManager;
 import android.content.Context;
+import android.util.ArrayMap;
 
 import com.android.internal.colorextraction.ColorExtractor.GradientColors;
 import com.android.internal.util.function.TriConsumer;
 
 import com.android.systemui.SystemUIFactory;
+import com.android.systemui.Dependency.DependencyProvider;
 import com.android.systemui.statusbar.NotificationLockscreenUserManager;
 import com.android.systemui.statusbar.ScrimView;
 import com.android.systemui.statusbar.phone.DozeParameters;
@@ -16,19 +18,20 @@ import com.android.systemui.statusbar.phone.LockscreenWallpaper;
 
 import java.util.function.Consumer;
 
-import com.android.systemui.statusbar.notification.NotificationEntryManager;
+import com.android.systemui.statusbar.NotificationEntryManager;
 
 import com.google.android.systemui.statusbar.NotificationEntryManagerGoogle;
 
 public class SystemUIGoogleFactory extends SystemUIFactory {
-    @Override
-    public NotificationEntryManager provideNotificationEntryManager(Context context) {
-        return new NotificationEntryManagerGoogle(context);
-    }
 
     @Override
-    public NotificationLockscreenUserManager provideNotificationLockscreenUserManager(Context context) {
-        return new NotificationLockscreenUserManagerGoogle(context);
+    public void injectDependencies(ArrayMap<Object, DependencyProvider> providers,
+            Context context) {
+        super.injectDependencies(providers, context);
+        providers.put(NotificationEntryManager.class,
+                () -> new NotificationEntryManagerGoogle(context));
+        providers.put(NotificationLockscreenUserManager.class,
+                () -> new NotificationLockscreenUserManagerGoogle(context));
     }
 
     @Override
