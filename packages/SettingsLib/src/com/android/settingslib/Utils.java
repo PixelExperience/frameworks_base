153b49ce0d80e540047d8ac661160fa974f37b11
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-21 13:07:23 +0000

    Revert "SystemUI: Implement weather on lockscreen like Pixels"
    This reverts commit 5499ee20eb12b3d4c21e70892d2eead086c487d1.
    
    Change-Id: I057b501fb622c29a9dc55d93f75bea70b20b59bc

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -30,8 +30,6 @@ import com.android.settingslib.drawable.UserIconDrawable;
 import com.android.settingslib.wrapper.LocationManagerWrapper;
 import java.text.NumberFormat;
 
-import com.android.internal.util.custom.weather.WeatherClient;
-
 import com.android.internal.util.custom.ambient.play.AmbientPlayHistoryManager;
 import com.android.internal.util.custom.ambient.play.AmbientPlayProvider;
 import com.android.internal.util.custom.thermal.ThermalController;
@@ -293,7 +291,6 @@ public class Utils {
                 || pkg.packageName.equals(sServicesSystemSharedLibPackageName)
                 || pkg.packageName.equals(sSharedSystemSharedLibPackageName)
                 || pkg.packageName.equals(PrintManager.PRINT_SPOOLER_PACKAGE_NAME)
-                || pkg.packageName.equals(WeatherClient.SERVICE_PACKAGE)
                 || pkg.packageName.equals(AmbientPlayHistoryManager.SERVICE_PACKAGE)
                 || pkg.packageName.equals(AmbientPlayProvider.SERVICE_PACKAGE)
                 || pkg.packageName.equals(ThermalController.SERVICE_PACKAGE)
