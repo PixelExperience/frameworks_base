31756cfc52ab1e2fb9dc08116b45214b369bcb8e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-10-09 15:07:19 +0000

    PhoneWindowManager: Disable three finger to screenshot on doze
    Change-Id: Ic7b38ffbe1980e78f9573dc2b0dc0b850d2c3ec2

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2358,6 +2358,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mSwipeToScreenshot = new SwipeToScreenshotListener(context, new SwipeToScreenshotListener.Callbacks() {
             @Override
             public void onSwipeThreeFinger() {
+                IDreamManager dreamManager = getDreamManager();
+                try {
+                    if (dreamManager != null && dreamManager.isDreaming()) {
+                        return;
+                    }
+                } catch (RemoteException ignored) {
+                }
                 mHandler.post(mScreenshotRunnable);
             }
         });
