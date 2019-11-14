59ffb64cba1f85e97b7dc9550565b53e76f4b739
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 14:07:44 +0530

    base: Add refresh rate setting [1/2]
    Change-Id: Ic9593b1bab4c0ff8842b3567d75f397a01e0acb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4520,6 +4520,11 @@ public final class Settings {
          */
 
         /**
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
