9b61e0f55e3fb173554e8dda07250943200200a6
Demon000 <demonsingur@gmail.com>
2020-01-19 21:54:24 +0000

    SystemUI: implement better partial screenshot
    Author: Demon000 <demonsingur@gmail.com>
    Date:   Mon Jan 20 03:37:29 2020 +0000
    
        ScreenshotHelper: ignore timeout when taking partial screenshots
    
        User should have as much time to select the region as he wants.
    
        Change-Id: Ib9b79d5127a59d3e2af278dbc8cd054221c37ca6
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sun Dec 1 18:19:48 2019 +0100
    
        SystemUI: block gestural navigation when using partial screenshot
    
        Change-Id: I68d25a6eab9ea16707e8736b9e56055532bcdf87
    
    Author: Demon000 <demonsingur@gmail.com>
    Date:   Sat Feb 1 18:24:34 2020 +0000
    
        GlobalScreenshot: do not capture multiple screenshots simultaneously
    
        Change-Id: I4c0c73759b8b0483f0d28b4c6c39e9a616c26b57
    
    Author: Demon000 <demonsingur@gmail.com>
    Date:   Thu Apr 9 22:59:06 2020 +0200
    
        ScreenshotSelector: handle rotation issues
    
        Disable rotation while the screenshot selector is visible,
        because it cannot handle rotation internally,
        and rotating while a certain selection is made might cause
        the selection to go off-screen.
    
        Also, adjust the crop coordinates so that they function properly.
        The native screenshot taking functions expect the coordinates
        to be in a physical left-to-right, top-to-bottom order, which
        is not the case for 90 degree rotation, where the top is
        actually the right physical edge, or for 270 degree rotation,
        where the right is actually the top physical edge, or for
        180 degrees, where both axes are flipped.
    
        https://gitlab.com/LineageOS/issues/android/-/issues/1749
    
        Change-Id: I18da90592cf7baa46547554df954bdb071961311
    
    Change-Id: I21936e96a5d3d89b2487beeec659ec360584d114

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -214,6 +214,8 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
     private static final int MAX_LOGGED_PREDICTIONS = 10;
     private ArrayDeque<String> mPredictionLog = new ArrayDeque<>();
 
+    private boolean mBlockedGesturalNavigation;
+
     private final GestureNavigationSettingsObserver mGestureNavigationSettingsObserver;
 
     private final NavigationEdgeBackPlugin.BackCallback mBackCallback =
@@ -797,6 +799,10 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         return topActivity != null && mGestureBlockingActivities.contains(topActivity);
     }
 
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
     @Override
     public void writeToProto(SystemUiTraceProto proto) {
         if (proto.edgeBackGestureHandler == null) {
@@ -811,7 +817,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         }
 
         public void onInputEvent(InputEvent event) {
-            EdgeBackGestureHandler.this.onInputEvent(event);
+            if (!mBlockedGesturalNavigation) {
+                EdgeBackGestureHandler.this.onInputEvent(event);
+            }
             finishInputEvent(event, true);
         }
     }
