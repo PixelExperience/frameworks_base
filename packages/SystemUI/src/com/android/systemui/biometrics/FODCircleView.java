579bcc1d8476fb7319839f6b12d11e4871cd4c56
00day0 <therandomuser11@gmail.com>
2020-01-20 15:50:39 +0100

    FODCircleView: fix incorrect position on keyguard
    Fixes bug when you watch video in youtube app in landscape and lock your device fod icon appears in incorrect position
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Change-Id: I8d46a313a4c903b698bb7804fc890237043bb3ca

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -76,6 +76,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     private boolean mIsBouncer;
     private boolean mIsDreaming;
+    private boolean mIsKeyguard;
     private boolean mIsShowing;
     private boolean mIsCircleShowing;
 
@@ -118,6 +119,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         @Override
         public void onKeyguardVisibilityChanged(boolean showing) {
+            mIsKeyguard = showing;
+            updatePosition();
             if (mFODAnimation != null) {
                 mFODAnimation.setAnimationKeyguard(showing);
             }
@@ -422,6 +425,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 break;
             default:
                 throw new IllegalArgumentException("Unknown rotation: " + rotation);
+        }
+
+        if (mIsKeyguard) {
+            mParams.x = mPositionX;
+            mParams.y = mPositionY - cutoutMaskedExtra;
         }
 
         if (mIsDreaming) {
