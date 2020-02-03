omnirom/android_frameworks_base_old

base: replace omni fingerprint with ro.build.date
the build date is unique for each build, so use that instead.

Change-Id: I9f7de51a4e36b011cc3a2d1058d8dec108cf05fb

core/java/android/os/Build.java
@@ -946,9 +946,10 @@ public class Build {
 
     /**
      * @hide
+     * originally stores the value of ro.omni.fingerprint
+     * replaced with ro.build.date but kept the field name to avoid having to rename everywhere
      */
-    public static final String OMNI_FINGERPRINT = SystemProperties.get("ro.omni.fingerprint",
-            deriveFingerprint());
+    public static final String OMNI_FINGERPRINT = getString("ro.build.date");
 
     /**
      * Some devices split the fingerprint components between multiple

PixelExperience/frameworks_base

core/java/android/os/Build.java
@@ -1001,18 +1001,8 @@ public class Build {
     /** A string that uniquely identifies this build.  Do not attempt to parse this value. */
     public static final String FINGERPRINT = deriveFingerprint();
 
-    /**
-     * Custom fingerprint
-     * @hide
-     */
-    public static final String PROP_CUSTOM_FINGERPRINT = "org.pixelexperience.fingerprint";
-
-    /**
-     * Custom fingerprint
-     * @hide
-     */
-    public static final String CUSTOM_FINGERPRINT = SystemProperties.get(PROP_CUSTOM_FINGERPRINT,
-            deriveFingerprint());
+    /** @hide */
+    public static final String CUSTOM_FINGERPRINT = getString("ro.build.date");
 
     /**
      * Some devices split the fingerprint components between multiple