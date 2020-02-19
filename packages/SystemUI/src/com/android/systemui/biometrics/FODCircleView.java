c31c391142366c80656b44a951ef06488eb0b633
Demon000 <demonsingur@gmail.com>
2020-02-19 18:53:59 -0500

    FODCircleView: update position of icon before show
    FOD icon can appear in the wrong position when locking
    the device while in landscape.
    Fix it by updating the position before showing.
    
    Change-Id: I812aa07357eadf8ff371761c041e44f5aab48046

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -426,6 +426,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         mIsShowing = true;
 
+        updatePosition();
+
         dispatchShow();
         setVisibility(View.VISIBLE);
     }
