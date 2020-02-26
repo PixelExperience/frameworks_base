de7288401ab10fd5a755d5491ec92346d7be8bde
Demon000 <demonsingur@gmail.com>
2020-02-26 10:07:30 -0500

    FODCircleView: split boost and dim into separate functions
    Change-Id: I889d7344deafda1b13c8321658006018b3a21603

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -338,7 +338,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         setKeepScreenOn(true);
 
-        setDim(true);
+        updateDim();
+        updateBoost();
         updateAlpha();
         dispatchPress();
 
@@ -358,7 +359,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         dispatchRelease();
 
-        setDim(false);
+        updateBoost();
+        updateDim();
         updateAlpha();
 
         setKeepScreenOn(false);
@@ -441,8 +443,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mWindowManager.updateViewLayout(this, mParams);
     }
 
-    private void setDim(boolean dim) {
-        if (dim) {
+    private void updateDim() {
+        if (mIsCircleShowing) {
             int curBrightness = Settings.System.getInt(getContext().getContentResolver(),
                     Settings.System.SCREEN_BRIGHTNESS, 100);
             int dimAmount = 0;
@@ -451,20 +453,27 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             try {
                 dimAmount = daemon.getDimAmount(curBrightness);
             } catch (RemoteException e) {
-                // do nothing
-            }
-
-            if (mShouldBoostBrightness) {
-                mParams.screenBrightness = 1.0f;
+                return;
             }
 
             mParams.dimAmount = dimAmount / 255.0f;
         } else {
-            mParams.screenBrightness = 0.0f;
             mParams.dimAmount = 0.0f;
         }
 
         mWindowManager.updateViewLayout(this, mParams);
+    }
+
+    private void updateBoost() {
+        if (mShouldBoostBrightness) {
+            if (mIsCircleShowing) {
+                mParams.screenBrightness = 1.0f;
+            } else {
+                mParams.screenBrightness = 0.0f;
+            }
+
+            mWindowManager.updateViewLayout(this, mParams);
+        }
     }
 
     private class BurnInProtectionTask extends TimerTask {
