86eeb50d86eabb59b7638157ffb90d021959bf78
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-09 08:48:26 -0300

    StatusBar: Disable centered clock if notch visible
    Change-Id: Ifea6e2e5119c38bed07cff896c483187642ccb17

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1966,6 +1966,9 @@ public class StatusBar extends SystemUI implements
     private void updateCutoutOverlay(boolean displayCutoutHidden) {
         boolean needsRefresh = mDisplayCutoutHidden != displayCutoutHidden;
         mDisplayCutoutHidden = displayCutoutHidden;
+        if (!mDisplayCutoutHidden && CutoutUtils.hasCenteredCutout(mContext, true) && isCenteredClock()){
+            moveClockToLeft();
+        }
         try {
             mOverlayManager.setEnabled("org.pixelexperience.overlay.hidecutout",
                         mDisplayCutoutHidden, mLockscreenUserManager.getCurrentUserId());
