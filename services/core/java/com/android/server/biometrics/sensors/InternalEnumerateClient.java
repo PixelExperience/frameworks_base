242263f396cafe166f9606f48bebc86334fa6c8e
Pierre-Hugues Husson <phh@phh.me>
2020-03-10 23:30:17 +0100

    Biometrics: Allow disabling of fingerprint cleanups
    [SamarV-121 - Adapt to S]
    
    Change-Id: I8a486e707712b81711fb1a691faec029499fa897
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- services/core/java/com/android/server/biometrics/sensors/InternalEnumerateClient.java
+++ services/core/java/com/android/server/biometrics/sensors/InternalEnumerateClient.java
@@ -43,6 +43,7 @@ public abstract class InternalEnumerateClient<T> extends HalClientMonitor<T>
     private List<? extends BiometricAuthenticator.Identifier> mEnrolledList;
     // List of templates to remove from the HAL
     private List<BiometricAuthenticator.Identifier> mUnknownHALTemplates = new ArrayList<>();
+    final boolean mNocleanup = android.os.SystemProperties.getBoolean("fingerprint.nocleanup", false);
 
     protected InternalEnumerateClient(@NonNull Context context, @NonNull LazyDaemon<T> lazyDaemon,
             @NonNull IBinder token, int userId, @NonNull String owner,
@@ -114,8 +115,10 @@ public abstract class InternalEnumerateClient<T> extends HalClientMonitor<T>
             BiometricAuthenticator.Identifier identifier = mEnrolledList.get(i);
             Slog.e(TAG, "doTemplateCleanup(): Removing dangling template from framework: "
                     + identifier.getBiometricId() + " " + identifier.getName());
-            mUtils.removeBiometricForUser(getContext(),
-                    getTargetUserId(), identifier.getBiometricId());
+           if (!mNocleanup) {
+              mUtils.removeBiometricForUser(getContext(),
+                       getTargetUserId(), identifier.getBiometricId());
+            }
             FrameworkStatsLog.write(FrameworkStatsLog.BIOMETRIC_SYSTEM_HEALTH_ISSUE_DETECTED,
                     mStatsModality,
                     BiometricsProtoEnums.ISSUE_UNKNOWN_TEMPLATE_ENROLLED_FRAMEWORK,
