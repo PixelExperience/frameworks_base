002062deaed218b987705f99c74fc7e64bcbaf27
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 22:48:17 -0300

    fwb: Guard some functions against face unlock
    Change-Id: I26d6f6d9ead884aff361236d2e5aa202d90704ba

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -656,11 +656,16 @@ public abstract class BiometricServiceBase extends SystemService
         mPowerManager = mContext.getSystemService(PowerManager.class);
         mUserManager = UserManager.get(mContext);
         mMetricsLogger = new MetricsLogger();
-        mCleanupUnusedFingerprints = mContext.getResources().getBoolean(
+        mCleanupUnusedFingerprints = statsModality() == BiometricsProtoEnums.MODALITY_FINGERPRINT &&
+                mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_cleanupUnusedFingerprints);
-        mNotifyClient = mContext.getResources().getBoolean(
+        mNotifyClient =
+                statsModality() == BiometricsProtoEnums.MODALITY_FINGERPRINT &&
+                mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_notifyClientOnFingerprintCancelSuccess);
-        mPostResetRunnableForAllClients = mContext.getResources().getBoolean(
+        mPostResetRunnableForAllClients =
+                statsModality() == BiometricsProtoEnums.MODALITY_FINGERPRINT &&
+                mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_fingerprintPostResetRunnableForAllClients);
     }
 
