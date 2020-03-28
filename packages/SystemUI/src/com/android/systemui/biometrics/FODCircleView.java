e8278a944054f127082f0cb1a6412cdd9bc3574f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 22:48:17 -0300

    fwb: Guard some functions against face unlock
    Change-Id: I26d6f6d9ead884aff361236d2e5aa202d90704ba

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -172,13 +172,16 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         @Override
         public void onBiometricRunningStateChanged(boolean running,
             BiometricSourceType biometricSourceType) {
-            dispatchFodFingerprintRunningStateChanged(running);
+            if (biometricSourceType == BiometricSourceType.FINGERPRINT){
+                dispatchFodFingerprintRunningStateChanged(running);
+            }
         }
 
         @Override
         public void onBiometricHelp(int msgId, String helpString,
                 BiometricSourceType biometricSourceType) {
-            if (msgId == -1){ // Auth error
+            if (biometricSourceType == BiometricSourceType.FINGERPRINT &&
+                    msgId == -1){ // Auth error
                 hideCircle();
                 mHandler.post(() -> mFODAnimation.hideFODanimation());
             }
