07e1f511b8b823064839e8859f1dff90f0d2c7f5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-28 10:42:55 -0300

    FodScreenOffHandler: Add hasEnrolledFingerprints callback
    Change-Id: I4caebc55d8c297101a5029c5b0bf291f9302e6d6

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -33,6 +33,7 @@ import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.drawable.AnimationDrawable;
 import android.hardware.biometrics.BiometricSourceType;
+import android.hardware.fingerprint.FingerprintManager;
 import android.graphics.PorterDuff;
 import android.os.Handler;
 import android.os.Looper;
@@ -119,6 +120,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         }
     };
 
+    private FingerprintManager mFingerprintManager;
     private KeyguardUpdateMonitor mUpdateMonitor;
 
     private KeyguardUpdateMonitorCallback mMonitorCallback = new KeyguardUpdateMonitorCallback() {
@@ -198,6 +200,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     };
 
     private void dispatchFodScreenStateChanged(boolean interactive){
+        dispatchFodFingerprintHasEnrolledFinger();
         if (mFodScreenOffHandler != null){
             mFodScreenOffHandler.onScreenStateChanged(interactive);
         }
@@ -212,6 +215,15 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private void dispatchFodDreamingStateChanged(){
         if (mFodScreenOffHandler != null){
             mFodScreenOffHandler.onDreamingStateChanged(mIsDreaming);
+        }
+    }
+
+    private void dispatchFodFingerprintHasEnrolledFinger(){
+        if (mFodScreenOffHandler != null &&
+                mFingerprintManager != null && 
+                mFingerprintManager.isHardwareDetected()) {
+            boolean enrolled = mFingerprintManager.hasEnrolledFingerprints(UserHandle.myUserId());
+            mFodScreenOffHandler.hasEnrolledFingerprints(enrolled);
         }
     }
 
@@ -277,6 +289,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     public FODCircleView(Context context, FodScreenOffHandler fodScreenOffHandler) {
         super(context);
 
+        mFingerprintManager = (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
         mFodScreenOffHandler = fodScreenOffHandler;
 
         setScaleType(ScaleType.CENTER);
