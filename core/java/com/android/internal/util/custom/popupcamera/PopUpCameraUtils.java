842ddab727f0ce5a4ab629c619c8298efaa2dd8f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-05 11:13:01 -0300

    framework: Implement light for popup camera
    Change-Id: I2ff7729ceb72303071b91f7e76a915baf33ea431

--- core/java/com/android/internal/util/custom/popupcamera/PopUpCameraUtils.java
+++ core/java/com/android/internal/util/custom/popupcamera/PopUpCameraUtils.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2019 The Pixel Experience Project
+* Copyright (C) 2020 The Pixel Experience Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,6 +15,34 @@
 */
 package com.android.internal.util.custom.popupcamera;
 
+import android.content.Context;
+import android.provider.Settings;
+import android.os.UserHandle;
+
 public class PopUpCameraUtils {
-    public static String MANAGE_POPUP_CAMERA_SERVICE_PERMISSION = "org.pixelexperience.device.MANAGE_POPUP_CAMERA_SERVICE";
+    public static String MANAGE_POPUP_CAMERA_SERVICE_PERMISSION =
+            "org.pixelexperience.device.MANAGE_POPUP_CAMERA_SERVICE";
+
+    public static boolean supportsLed(Context context) {
+        return context.getResources().getBoolean(
+                com.android.internal.R.bool.config_popUpNotificationLight);
+    }
+
+    public static boolean isLedEnabled(Context context) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_LED_ENABLED, 1,
+                UserHandle.USER_CURRENT) != 0;
+    }
+
+    public static void setLedEnabled(Context context, boolean enabled) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_LED_ENABLED, enabled ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
+
+    public static void blockBatteryLed(Context context, boolean blocked) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED, blocked ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
 }
