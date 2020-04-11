1193b8980fb1d145b8a2601ea1a8848ed4e0565b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-11 11:00:35 -0300

    NavigationBarView: Show back button on bouncer if gestures disabled
    Change-Id: If581aa673d9365a4589f9293337fc1328916c70c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -725,9 +725,10 @@ public class NavigationBarView extends FrameLayout implements
             disableHome = true;
             disableHomeHandle = true;
             disableRecent = true;
+            disableBack = true;
         }
 
-        if (mKeyguardBouncerShowing){
+        if (mKeyguardBouncerShowing && !isGesturalMode(mNavBarMode)){
             disableBack = false;
         }
 
