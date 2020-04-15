6b9e4687aec1ec75100766ddf9934a8cb82cf13d
DarkJoker360 <simoespo159@gmail.com>
2020-04-15 15:09:40 +0000

    Ask pin for shutdown/reboot on secure lockscreen
    Based on: https://github.com/ezio84/abc_frameworks_base/commit/62537d0ea76331fe838f25732fbe38c69eb0904e
    
    @neobuddy89:
    * Code clean up, optimization and disabled by default.
    * Adapted for Android 11
    
    @jhenrique09 edits:
    
    * Remove check for keyguard if advanced restart is enabled
    
    Change-Id: I668b36669905bbc664e3a3a4244f5235dde11881

--- core/java/com/android/internal/util/custom/globalactions/PowerMenuUtils.java
+++ core/java/com/android/internal/util/custom/globalactions/PowerMenuUtils.java
@@ -16,19 +16,16 @@
 
 package com.android.internal.util.custom.globalactions;
 
-import android.app.KeyguardManager;
 import android.content.Context;
 import android.os.UserHandle;
 import android.provider.Settings;
 
 public final class PowerMenuUtils {
     public static boolean isAdvancedRestartPossible(final Context context) {
-        KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
-        boolean keyguardLocked = km.inKeyguardRestrictedInputMode() && km.isKeyguardSecure();
         boolean advancedRestartEnabled = Settings.Secure.getInt(context.getContentResolver(),
                 Settings.Secure.ADVANCED_REBOOT, 0) == 1;
         boolean isPrimaryUser = UserHandle.getCallingUserId() == UserHandle.USER_SYSTEM;
 
-        return advancedRestartEnabled && !keyguardLocked && isPrimaryUser;
+        return advancedRestartEnabled && isPrimaryUser;
     }
 }
