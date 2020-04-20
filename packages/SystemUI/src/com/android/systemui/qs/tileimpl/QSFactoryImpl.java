fa2b42ed9c94e3de9f255ad36ecc81ebe7abd5f3
Linux4 <tim@linux4.de>
2020-04-20 17:57:43 +0200

    SystemUI: Add powershare QS tile
    Author: Linux4 <tim@linux4.de>
    Date:   Mon Apr 20 17:57:43 2020 +0200
    
        base: SystemUI: add qs powershare tile
    
        Change-Id: Ibcf5cc3e766d971d9dca9242bf2770093d4d3099
    
    Author: Linux4 <tim@linux4.de>
    Date:   Sat May 2 12:54:49 2020 +0200
    
        base: SystemUI: fix NPE in PowerShareTile
    
        Change-Id: If69846f3e3d4f3a12c0f78f47b5adc1008888de1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Jul 7 13:27:12 2020 +0200
    
        SystemUI: Call setOnlyAlertOnce(true) for PowerShare notification
    
        * Otherwise we end up getting notification sound
          every 20 seconds when using it...
    
        Change-Id: Ia56b01f332860b64f0d2c532884e6bcaa2acfb17
    
    Change-Id: Ia09a674745adb433f3aaeebfeb79899906350793

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MicrophoneToggleTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.PowerShareTile;
 import com.android.systemui.qs.tiles.QuickAccessWalletTile;
 import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
@@ -90,6 +91,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<DeviceControlsTile> mDeviceControlsTileProvider;
     private final Provider<AlarmTile> mAlarmTileProvider;
     private final Provider<QuickAccessWalletTile> mQuickAccessWalletTileProvider;
+    private final Provider<PowerShareTile> mPowerShareTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -124,7 +126,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<MicrophoneToggleTile> microphoneToggleTileProvider,
             Provider<DeviceControlsTile> deviceControlsTileProvider,
             Provider<AlarmTile> alarmTileProvider,
-            Provider<QuickAccessWalletTile> quickAccessWalletTileProvider) {
+            Provider<QuickAccessWalletTile> quickAccessWalletTileProvider,
+            Provider<PowerShareTile> powerShareTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -155,6 +158,7 @@ public class QSFactoryImpl implements QSFactory {
         mDeviceControlsTileProvider = deviceControlsTileProvider;
         mAlarmTileProvider = alarmTileProvider;
         mQuickAccessWalletTileProvider = quickAccessWalletTileProvider;
+        mPowerShareTileProvider = powerShareTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -221,8 +225,10 @@ public class QSFactoryImpl implements QSFactory {
                 return mAlarmTileProvider.get();
             case "wallet":
                 return mQuickAccessWalletTileProvider.get();
+            // Additional tiles.
+            case "powershare":
+                return mPowerShareTileProvider.get();
         }
-
         // Custom tiles
         if (tileSpec.startsWith(CustomTile.PREFIX)) {
             return CustomTile.create(
