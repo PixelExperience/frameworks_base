1c01c383c0a10466725b3d5bf061042d86a0f53a
Chandra Chaganti <chandra.chaganti@hotmail.com>
2020-05-02 11:42:43 +0200

    FOD: Revert commits to use lineage pattern/pin view
    Revert "SystemUI: Restore keyguard dimens after b40a3db"
    
    This reverts commit df5fa4c.
    
    Revert "FOD: Show fod circle below pattern/pin view"
    
    This reverts commit b40a3db.
    
    Change-Id: Ic3042add77c712fc835da99bed4f7e6bab60d6d3

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -47,7 +47,6 @@ import android.widget.ImageView;
 
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
-import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
 import com.android.systemui.R;
 import com.android.systemui.Dependency;
 import com.android.systemui.statusbar.policy.ConfigurationController;
@@ -135,17 +134,10 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         public void onKeyguardBouncerChanged(boolean isBouncer) {
             mIsBouncer = isBouncer;
 
-            if (mIsKeyguard && mUpdateMonitor.isFingerprintDetectionRunning()) {
-                final SecurityMode sec = mUpdateMonitor.getSecurityMode();
-                final boolean maybeShow = sec == SecurityMode.Pattern ||
-                        sec == SecurityMode.PIN;
-                if (maybeShow || !mIsBouncer) {
-                    show();
-                } else {
-                    hide();
-                }
-            } else {
+            if (isBouncer) {
                 hide();
+            } else if (mUpdateMonitor.isFingerprintDetectionRunning()) {
+                show();
             }
         }
 
