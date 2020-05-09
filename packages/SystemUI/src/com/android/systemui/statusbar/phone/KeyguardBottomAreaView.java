8762343632813a316d31fe2633500d16a5a902b7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-05-09 13:40:43 -0300

    Revert old impl of show fod circle below pattern/pin view
    Revert "SystemUI: Keyguard: Only apply padding if fod view showing"
    
    This reverts commit 7682dc617fd101a0fba38c0f6a87a5a7a1bfcccc.
    
    Revert "FOD: Show fod circle below pattern/pin view"
    
    This reverts commit b40a3db3313f13b2af86f818a83afbb5e777be55.
    
    Change-Id: If75211f85ae8e7631a9b47b878ef9595d0572dc1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -73,7 +73,6 @@ import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.assist.AssistManager;
-import com.android.systemui.biometrics.FODCircleView;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.IntentButtonProvider;
 import com.android.systemui.plugins.IntentButtonProvider.IntentButton;
@@ -417,8 +416,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     }
 
     private boolean hasInDisplayFingerprint() {
-        return FodUtils.hasFodSupport(mContext) && mIsFingerprintRunning &&
-            FODCircleView.canUnlockWithFp(KeyguardUpdateMonitor.getInstance(mContext));
+        return FodUtils.hasFodSupport(mContext) && mIsFingerprintRunning;
     }
 
     public boolean isLeftVoiceAssist() {
