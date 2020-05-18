645f4cbea6552565943694df9859591a34dfe9bf
rituj <ritujbeniwal@gmail.com>
2020-05-18 12:23:21 +0530

    SystemUI: BiometricPrompt: Adjust layout if FOD is present
    * Also force portrait mode if FOD is present
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: I0b68b1ee6c5a8031deb195fbdb0b23c33b0ab590

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthContainerView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthContainerView.java
@@ -20,6 +20,7 @@ import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.hardware.biometrics.BiometricAuthenticator;
 import android.hardware.biometrics.BiometricConstants;
@@ -460,10 +461,12 @@ public class AuthContainerView extends LinearLayout
 
     @Override
     public void show(WindowManager wm, @Nullable Bundle savedState) {
+        boolean hasFod = false;
         if (mBiometricView != null) {
             mBiometricView.restoreState(savedState);
+            hasFod = mBiometricView.getHasFod();
         }
-        wm.addView(this, getLayoutParams(mWindowToken));
+        wm.addView(this, getLayoutParams(mWindowToken, hasFod));
     }
 
     @Override
@@ -623,7 +626,7 @@ public class AuthContainerView extends LinearLayout
      * @param windowToken token for the window
      * @return
      */
-    public static WindowManager.LayoutParams getLayoutParams(IBinder windowToken) {
+    public static WindowManager.LayoutParams getLayoutParams(IBinder windowToken, boolean hasFod) {
         final int windowFlags = WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
                 | WindowManager.LayoutParams.FLAG_SECURE;
         final WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
@@ -636,6 +639,9 @@ public class AuthContainerView extends LinearLayout
         lp.setFitInsetsTypes(lp.getFitInsetsTypes() & ~WindowInsets.Type.ime());
         lp.setTitle("BiometricPrompt");
         lp.token = windowToken;
+        if (hasFod) {
+            lp.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        }
         return lp;
     }
 }
