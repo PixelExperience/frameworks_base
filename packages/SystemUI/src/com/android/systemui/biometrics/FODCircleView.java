d602b9f473d1d173135c2afefac98d5f16f491f1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-02 23:06:42 -0300

    Revert "FODCircleView: Fix high brightness after unlocking"
    This reverts commit 82d00dac3dfdba5109b1b2817e891959b4433081.
    
    Change-Id: Ib5be71ed6d0aa045a6d7c62f19b92ac0696d52b1

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -138,7 +138,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         @Override
         public void onKeyguardBouncerChanged(boolean isBouncer) {
             mIsBouncer = isBouncer;
-            if (mIsKeyguard && mUpdateMonitor.isFingerprintDetectionRunning()) {
+            if (mUpdateMonitor.isFingerprintDetectionRunning()) {
                 if (isPinOrPattern(mUpdateMonitor.getCurrentUser()) || !isBouncer) {
                     show();
                 } else {
