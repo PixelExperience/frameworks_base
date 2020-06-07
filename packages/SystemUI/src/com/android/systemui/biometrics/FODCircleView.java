04b08e31723f1eecd009c34f4b2a23808cfa40cc
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2020-06-07 13:56:30 +0200

    FODCircleView: Dim FOD icon
    Change-Id: Ic8bb03ae2af68c877f41510068f3d9e307193384
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -26,6 +26,7 @@ import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.hardware.biometrics.BiometricSourceType;
+import android.graphics.PorterDuff;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
@@ -56,12 +57,34 @@ import java.util.TimerTask;
 
 public class FODCircleView extends ImageView implements ConfigurationListener {
     private static final int FADE_ANIM_DURATION = 250;
+    private final String SCREEN_BRIGHTNESS = "system:" + Settings.System.SCREEN_BRIGHTNESS;
+    private final int[][] BRIGHTNESS_ALPHA_ARRAY = {
+        new int[]{0, 255},
+        new int[]{1, 224},
+        new int[]{2, 213},
+        new int[]{3, 211},
+        new int[]{4, 208},
+        new int[]{5, 206},
+        new int[]{6, 203},
+        new int[]{8, 200},
+        new int[]{10, 196},
+        new int[]{15, 186},
+        new int[]{20, 176},
+        new int[]{30, 160},
+        new int[]{45, 139},
+        new int[]{70, 114},
+        new int[]{100, 90},
+        new int[]{150, 56},
+        new int[]{227, 14},
+        new int[]{255, 0}
+    };
     private final int mPositionX;
     private final int mPositionY;
     private final int mSize;
     private final int mDreamingMaxOffset;
     private final int mNavigationBarSize;
     private final boolean mShouldBoostBrightness;
+    private final boolean mTargetUsesInKernelDimming;
     private final Paint mPaintFingerprintBackground = new Paint();
     private final Paint mPaintFingerprint = new Paint();
     private final WindowManager.LayoutParams mParams = new WindowManager.LayoutParams();
@@ -70,6 +93,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     private IFingerprintInscreen mFingerprintInscreenDaemon;
 
+    private int mCurrentBrightness;
     private int mDreamingOffsetX;
     private int mDreamingOffsetY;
 
@@ -215,6 +239,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mPaintFingerprint.setAntiAlias(true);
 
         mPaintFingerprintBackground.setColor(res.getColor(R.color.config_fodColorBackground));
+        mTargetUsesInKernelDimming = res.getBoolean(com.android.internal.R.bool.config_targetUsesInKernelDimming);
         mPaintFingerprintBackground.setAntiAlias(true);
 
         mWindowManager = context.getSystemService(WindowManager.class);
@@ -266,6 +291,46 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     
         updateCutoutFlags();
         Dependency.get(ConfigurationController.class).addCallback(this);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        mCurrentBrightness = newValue != null ? Integer.parseInt(newValue) : 0;
+        updateIconDim();
+    }
+
+    private int interpolate(int i, int i2, int i3, int i4, int i5) {
+        int i6 = i5 - i4;
+        int i7 = i - i2;
+        int i8 = ((i6 * 2) * i7) / (i3 - i2);
+        int i9 = i8 / 2;
+        int i10 = i2 - i3;
+        return i4 + i9 + (i8 % 2) + ((i10 == 0 || i6 == 0) ? 0 : (((i7 * 2) * (i - i3)) / i6) / i10);
+    }
+
+    private int getDimAlpha() {
+        int length = BRIGHTNESS_ALPHA_ARRAY.length;
+        int i = 0;
+        while (i < length && BRIGHTNESS_ALPHA_ARRAY[i][0] < mCurrentBrightness) {
+            i++;
+        }
+        if (i == 0) {
+            return BRIGHTNESS_ALPHA_ARRAY[0][1];
+        }
+        if (i == length) {
+            return BRIGHTNESS_ALPHA_ARRAY[length - 1][1];
+        }
+        int[][] iArr = BRIGHTNESS_ALPHA_ARRAY;
+        int i2 = i - 1;
+        return interpolate(mCurrentBrightness, iArr[i2][0], iArr[i][0], iArr[i2][1], iArr[i][1]);
+    }
+
+    public void updateIconDim() {
+        if (!mIsCircleShowing && mTargetUsesInKernelDimming) {
+            setColorFilter(Color.argb(getDimAlpha(), 0, 0, 0), PorterDuff.Mode.SRC_ATOP);
+        } else {
+            setColorFilter(Color.argb(0, 0, 0, 0), PorterDuff.Mode.SRC_ATOP);
+        }
     }
 
     @Override
@@ -371,6 +436,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         dispatchPress();
 
         setImageDrawable(null);
+        updateIconDim();
         invalidate();
     }
 
