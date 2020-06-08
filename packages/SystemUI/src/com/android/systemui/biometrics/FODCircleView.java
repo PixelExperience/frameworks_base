6bc1e743917eceff1d759d5265ea15a1f47a6e21
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-06-08 21:23:08 +0000

    FODCircleView: let's smoothly fade-out the view, before going to View.GONE
    Tests executed:
        - Ensured animation executes correctly
        - Ensured interaction with FOD (or any other biometric method) wouldn't compromise animation execution or with FOD functionality itself
        - Ensured that alpha values triggered with the animation, are correct over different lockscreen states
    
    Change-Id: Ic4c8160726e66b4f9a43ff28709d03d5b6e262de
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -62,6 +62,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class FODCircleView extends ImageView implements ConfigurationListener {
+    private static final int FADE_ANIM_DURATION = 250;
     private final int mPositionX;
     private final int mPositionY;
     private final int mSize;
@@ -83,6 +84,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private int mColorBackground;
 
     private boolean mIsBiometricRunning;
+    private boolean mFading;
     private boolean mIsBouncer;
     private boolean mIsDreaming;
     private boolean mIsKeyguard;
@@ -363,6 +365,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     }
 
     public void dispatchPress() {
+        if (mFading) return;
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         try {
             daemon.onPress();
@@ -399,6 +402,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     }
 
     public void showCircle() {
+        if (mFading) return;
         mIsCircleShowing = true;
 
         setKeepScreenOn(true);
@@ -446,12 +450,25 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         updatePosition();
 
-        dispatchShow();
         setVisibility(View.VISIBLE);
+        animate().withStartAction(() -> mFading = true)
+                .alpha(mIsDreaming ? 0.5f : 1.0f)
+                .setDuration(FADE_ANIM_DURATION)
+                .withEndAction(() -> mFading = false)
+                .start();
+        dispatchShow();
     }
 
     public void hide() {
-        setVisibility(View.GONE);
+        animate().withStartAction(() -> mFading = true)
+                .alpha(0)
+                .setDuration(FADE_ANIM_DURATION)
+                .withEndAction(() -> {
+                    setVisibility(View.GONE);
+                    mFading = false;
+                })
+                .start();
+
         hideCircle();
         dispatchHide();
     }
