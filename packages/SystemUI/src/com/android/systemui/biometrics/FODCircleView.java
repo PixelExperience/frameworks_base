9212060c9ef28f67bbe695a5dfddb343f87f643b
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-07-05 22:58:01 +0300

    FODCircleView: Post calls of main hal functions on background thread
    Change-Id: Ie3175fd143a83a87f985e3cb3a668b9d589c5903
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -43,6 +43,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
+import com.android.settingslib.utils.ThreadUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.ConfigurationController;
@@ -439,7 +440,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener,Tu
         setKeepScreenOn(true);
 
         setDim(true);
-        dispatchPress();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchPress();
+        });
 
         setImageDrawable(null);
         updateIconDim();
@@ -452,7 +455,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener,Tu
         setImageResource(R.drawable.fod_icon_default);
         invalidate();
 
-        dispatchRelease();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchRelease();
+        });
         setDim(false);
 
         setKeepScreenOn(false);
@@ -492,7 +497,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener,Tu
                 .setDuration(FADE_ANIM_DURATION)
                 .withEndAction(() -> mFading = false)
                 .start();
-        dispatchShow();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchShow();
+        });
     }
 
     public void hide() {
@@ -506,7 +513,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener,Tu
                 .start();
         Dependency.get(TunerService.class).removeTunable(this);
         hideCircle();
-        dispatchHide();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchHide();
+        });
     }
 
     private void updateAlpha() {
