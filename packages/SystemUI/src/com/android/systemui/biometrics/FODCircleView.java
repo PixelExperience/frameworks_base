d2ea7b4f785ef4d2911e76bd0dab5b42c5608d84
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-17 16:12:24 -0300

    Revert "FODCircleView: Improve code style"
    This reverts commit e5b817e939218628ff8756ed4cf90c3d848a1691.
    
    Change-Id: I90ab3b4d36aeafc854e14cef7df25e6967480367

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -67,8 +67,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class FODCircleView extends ImageView implements ConfigurationListener {
-    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
-
     private final int mPositionX;
     private final int mPositionY;
     private final int mSize;
@@ -101,6 +99,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private boolean mPressPending;
     private boolean mScreenTurnedOn;
 
+    private Context mContext;
     private PowerManager mPowerManager;
     private PowerManager.WakeLock mWakeLock;
 
@@ -118,7 +117,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         public void onFingerDown() {
             if (mSupportsFodGesture && mFodGestureEnable && !mScreenTurnedOn) {
                 if (mDozeEnabled) {
-                    mHandler.post(() -> mContext.sendBroadcast(new Intent(DOZE_INTENT)));
+                    mHandler.post(() -> mContext.sendBroadcast(new Intent("com.android.systemui.doze.pulse")));
                 } else {
                     mWakeLock.acquire(3000);
                     mHandler.post(() -> mPowerManager.wakeUp(SystemClock.uptimeMillis(),
@@ -240,6 +239,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         FodGestureSettingsObserver(Context context, Handler handler) {
             super(handler);
+            mContext = context;
         }
 
         void registerListener() {
@@ -278,6 +278,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     public FODCircleView(Context context) {
         super(context);
+
+        mContext = context;
 
         setScaleType(ScaleType.CENTER);
 
