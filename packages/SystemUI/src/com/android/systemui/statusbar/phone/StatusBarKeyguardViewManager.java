e1ec1f9cc567650a1eed50ab364c9f55b8330bb1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-09-06 16:42:27 -0300

    Improve face unlock behavior on devices with popup camera
    Revert "fixup - KeyguardUpdateMonitor: Fix race condition before listening for biometrics"
    
    This reverts commit 5f9e830026038ac56f84872e7c7da08b999d6150.
    
    Revert "KeyguardUpdateMonitor: Fix race condition before listening for biometrics"
    
    This reverts commit e53b9b3b903e040da75970a154c0731515cac15e.
    
    Change-Id: I5d41270f8cfb2fb5cd66ab681d96804ebe7467db

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -98,6 +98,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
             updateStates();
             mStatusBar.wakeUpIfDozing(SystemClock.uptimeMillis(), mContainer, "BOUNCER_VISIBLE");
             updateLockIcon();
+            onKeyguardBouncerFullyShownChanged(true);
         }
 
         @Override
@@ -115,6 +116,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         public void onFullyHidden() {
             updateStates();
             updateLockIcon();
+            onKeyguardBouncerFullyShownChanged(false);
         }
     };
     private final DockManager.DockEventListener mDockEventListener =
@@ -772,6 +774,11 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
             mStatusBar.getNavigationBarView().getRootView().setVisibility(View.VISIBLE);
         }
     };
+
+    private void onKeyguardBouncerFullyShownChanged(boolean fullyShown){
+        KeyguardUpdateMonitor updateMonitor = KeyguardUpdateMonitor.getInstance(mContext);
+        updateMonitor.onKeyguardBouncerFullyShown(fullyShown);
+    }
 
     protected void updateStates() {
         int vis = mContainer.getSystemUiVisibility();
