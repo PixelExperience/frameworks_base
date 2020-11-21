6a8cf1e8dc1f30db3244811528fe4be3b70c2ccc
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-21 21:20:23 +0200

    FODCircleView: Make sure we hide when keyguard is dismissed
    Some things are weird, some users are able to make it stay
    Also improve some code all around the class:
    * Update icon dim initially
    * Only update dim for brightness when brightness actually changed
    * Shorten dim animation duration
    * Use background thread for a setColorFilter we missed
    * Get rid of a useless function for setting alpha on doze, it won't work anyways
    
    Change-Id: Ife48fc10bff0dd2313ab27afff4e4ce45e274231
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -159,11 +159,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         @Override
         public void onDreamingStateChanged(boolean dreaming) {
             mIsDreaming = dreaming;
-            updateAlpha();
+            updateIconDim(false);
 
             if (mIsKeyguard && mUpdateMonitor.isFingerprintDetectionRunning()) {
                 show();
-                updateAlpha();
+                updateIconDim(false);
             } else {
                 hide();
             }
@@ -173,6 +173,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 mBurnInProtectionTimer.schedule(new BurnInProtectionTask(), 0, 60 * 1000);
             } else if (mBurnInProtectionTimer != null) {
                 mBurnInProtectionTimer.cancel();
+                mBurnInProtectionTimer = null;
                 updatePosition();
             }
         }
@@ -183,7 +184,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             if (!showing) {
                 hide();
             } else {
-                updateAlpha();
+                updateIconDim(false);
             }
         }
 
@@ -246,9 +247,12 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         }
 
         void update() {
-            mCurrentBrightness = Settings.System.getInt(
+            int brightness = Settings.System.getInt(
                     mContext.getContentResolver(), SCREEN_BRIGHTNESS, 100);
-            updateIconDim(false);
+            if (mCurrentBrightness != brightness) {
+                mCurrentBrightness = brightness;
+                updateIconDim(false);
+            }
         }
     }
 
@@ -280,8 +284,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mPaintFingerprint.setAntiAlias(true);
 
         mPaintFingerprintBackground.setColor(res.getColor(R.color.config_fodColorBackground));
-        mTargetUsesInKernelDimming = res.getBoolean(com.android.internal.R.bool.config_targetUsesInKernelDimming);
         mPaintFingerprintBackground.setAntiAlias(true);
+
+        mTargetUsesInKernelDimming = res.getBoolean(com.android.internal.R.bool.config_targetUsesInKernelDimming);
 
         mWindowManager = context.getSystemService(WindowManager.class);
 
@@ -292,6 +297,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mHandler = new Handler(Looper.getMainLooper());
 
         mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+        mCustomSettingsObserver.update();
 
         mParams.height = mSize;
         mParams.width = mSize;
@@ -382,11 +388,13 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                         mIsAnimating = false;
                     }
                 });
-                anim.setDuration(1000);
+                anim.setDuration(500);
                 mIsAnimating = true;
                 mHandler.post(() -> anim.start());
             } else if (!mIsAnimating) {
-                setColorFilter(Color.argb(getDimAlpha(), 0, 0, 0), PorterDuff.Mode.SRC_ATOP);
+                mHandler.post(() ->
+                        setColorFilter(Color.argb(getDimAlpha(), 0, 0, 0),
+                        PorterDuff.Mode.SRC_ATOP));
             }
         } else {
             mHandler.post(() -> setColorFilter(Color.argb(0, 0, 0, 0), PorterDuff.Mode.SRC_ATOP));
@@ -566,10 +574,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         ThreadUtils.postOnBackgroundThread(() -> {
             dispatchHide();
         });
-    }
-
-    private void updateAlpha() {
-        setAlpha(mIsDreaming ? 0.5f : 1.0f);
     }
 
     private void updatePosition() {
