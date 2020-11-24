390c1ee19391bb57c78e354d0099c2cd534e5cb2
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-24 19:49:53 -0300

    SystemUI: Implement Smart space from redfin 11
    Includes weather on lockscreen and cool stuff on ambient display
    
    Code reverse engineered from redfin RD1A.201105.003.C1
    
    Requires Google app installed
    
    Change-Id: Ice5ea4fd43412cf916daefa6521fc8b55f14f199

--- packages/SystemUI/src/com/google/android/systemui/NotificationLockscreenUserManagerGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/NotificationLockscreenUserManagerGoogle.java
@@ -6,12 +6,16 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.UserManager;
 import com.android.systemui.broadcast.BroadcastDispatcher;
+import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.NotificationClickNotifier;
 import com.android.systemui.statusbar.NotificationLockscreenUserManagerImpl;
 import com.android.systemui.statusbar.phone.KeyguardBypassController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
+
+import com.google.android.systemui.smartspace.SmartSpaceController;
+
 import dagger.Lazy;
 
 import javax.inject.Inject;
@@ -20,6 +24,54 @@ import javax.inject.Singleton;
 @Singleton
 public class NotificationLockscreenUserManagerGoogle extends NotificationLockscreenUserManagerImpl {
     @Inject
-    public NotificationLockscreenUserManagerGoogle(Context context, BroadcastDispatcher broadcastDispatcher, DevicePolicyManager devicePolicyManager, UserManager userManager, NotificationClickNotifier notificationClickNotifier, KeyguardManager keyguardManager, StatusBarStateController statusBarStateController, Handler handler, DeviceProvisionedController deviceProvisionedController, KeyguardStateController keyguardStateController, Lazy<KeyguardBypassController> lazy) {
-        super(context, broadcastDispatcher, devicePolicyManager, userManager, notificationClickNotifier, keyguardManager, statusBarStateController, handler, deviceProvisionedController, keyguardStateController);    }
+    public Lazy<KeyguardBypassController> mKeyguardBypassControllerLazy;
+    private final KeyguardStateController mKeyguardStateController;
+    @Inject
+    public SmartSpaceController mSmartSpaceController;
+
+    @Inject
+    public NotificationLockscreenUserManagerGoogle(Context context,
+            BroadcastDispatcher broadcastDispatcher,
+            DevicePolicyManager devicePolicyManager,
+            UserManager userManager,
+            NotificationClickNotifier clickNotifier,
+            KeyguardManager keyguardManager,
+            StatusBarStateController statusBarStateController,
+            @Main Handler mainHandler,
+            DeviceProvisionedController deviceProvisionedController,
+            KeyguardStateController keyguardStateController) {
+        super(context, broadcastDispatcher, devicePolicyManager, userManager,
+            clickNotifier, keyguardManager, statusBarStateController, mainHandler,
+            deviceProvisionedController, keyguardStateController);
+        mKeyguardStateController = keyguardStateController;
+        mKeyguardStateController.addCallback(new KeyguardStateController.Callback() {
+            @Override 
+            public void onKeyguardShowingChanged() {
+                updateSmartSpaceVisibilitySettings();
+            }
+        });
+    }
+
+    @Override
+    public void updateLockscreenNotificationSetting() {
+        super.updateLockscreenNotificationSetting();
+        updateSmartSpaceVisibilitySettings();
+    }
+
+    public void updateSmartSpaceVisibilitySettings() {
+        boolean hideSensitiveData = !userAllowsPrivateNotificationsInPublic(mCurrentUserId) && (isAnyProfilePublicMode() || !mKeyguardStateController.isShowing());
+        boolean hideWorkData = false;
+        if (!mKeyguardBypassControllerLazy.get().getBypassEnabled()) {
+            if (!allowsManagedPrivateNotificationsInPublic() && (isAnyManagedProfilePublicMode() || !mKeyguardStateController.isShowing())) {
+                hideWorkData = true;
+            }
+        }
+        mSmartSpaceController.setHideSensitiveData(hideSensitiveData, hideWorkData);
+    }
+
+    @Override
+    public void updatePublicMode() {
+        super.updatePublicMode();
+        updateLockscreenNotificationSetting();
+    }
 }
--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -6,85 +6,97 @@ import android.annotation.Nullable;
 import android.content.Context;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.SystemClock;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
-import com.android.keyguard.KeyguardUpdateMonitor;
-import com.android.systemui.dagger.qualifiers.UiBackground;
-import com.android.systemui.statusbar.notification.init.NotificationsController;
-import com.android.systemui.statusbar.phone.LightBarController;
-import com.android.systemui.statusbar.phone.AutoHideController;
-import com.android.systemui.statusbar.phone.StatusBarIconController;
-import com.android.systemui.statusbar.PulseExpansionHandler;
-import com.android.systemui.statusbar.notification.NotificationWakeUpCoordinator;
-import com.android.systemui.statusbar.phone.KeyguardBypassController;
-import com.android.systemui.statusbar.policy.KeyguardStateController;
-import com.android.systemui.statusbar.phone.HeadsUpManagerPhone;
-import com.android.systemui.statusbar.notification.DynamicPrivacyController;
-import com.android.systemui.statusbar.notification.interruption.BypassHeadsUpNotifier;
-import com.android.systemui.plugins.FalsingManager;
-import com.android.systemui.broadcast.BroadcastDispatcher;
-import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
-import com.android.systemui.statusbar.notification.row.NotificationGutsManager;
-import com.android.systemui.statusbar.NotificationRemoteInputManager;
-import com.android.systemui.statusbar.notification.logging.NotificationLogger;
-import com.android.systemui.statusbar.notification.interruption.NotificationInterruptStateProvider;
-import com.android.systemui.statusbar.NotificationViewHierarchyManager;
-import com.android.systemui.keyguard.KeyguardViewMediator;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import com.android.internal.logging.MetricsLogger;
-import com.android.systemui.statusbar.NotificationMediaManager;
-import com.android.systemui.statusbar.NotificationLockscreenUserManager;
-import com.android.systemui.statusbar.policy.UserSwitcherController;
-import com.android.systemui.statusbar.policy.NetworkController;
-import com.android.systemui.statusbar.policy.BatteryController;
+import com.android.keyguard.KeyguardUpdateMonitor;
+import com.android.keyguard.ViewMediatorCallback;
+import com.android.systemui.dagger.qualifiers.UiBackground;
+import com.android.systemui.Dependency;
+import com.android.systemui.InitController;
+import com.android.systemui.assist.AssistManager;
+import com.android.systemui.broadcast.BroadcastDispatcher;
+import com.android.systemui.bubbles.BubbleController;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
+import com.android.systemui.dock.DockManager;
+import com.android.systemui.keyguard.DismissCallbackRegistry;
+import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
-import com.android.systemui.statusbar.SysuiStatusBarStateController;
-import com.android.systemui.statusbar.VibratorHelper;
-import com.android.systemui.bubbles.BubbleController;
-import com.android.systemui.statusbar.phone.NotificationGroupManager;
-import com.android.systemui.statusbar.notification.VisualStabilityManager;
-import com.android.systemui.statusbar.policy.DeviceProvisionedController;
-import com.android.systemui.statusbar.NavigationBarController;
-import com.android.systemui.assist.AssistManager;
-import com.android.systemui.statusbar.phone.BiometricUnlockController;
-import com.android.systemui.statusbar.policy.ConfigurationController;
-import com.android.systemui.statusbar.phone.NotificationShadeWindowController;
-import com.android.systemui.statusbar.phone.LockscreenLockIconController;
-import com.android.systemui.statusbar.phone.DozeParameters;
-import com.android.systemui.statusbar.phone.KeyguardLiftController;
-import com.android.systemui.statusbar.phone.LockscreenWallpaper;
-import com.android.systemui.statusbar.phone.DozeServiceHost;
-import com.android.systemui.recents.ScreenPinningRequest;
-import com.android.systemui.statusbar.phone.DozeScrimController;
-import com.android.systemui.volume.VolumeComponent;
-import com.android.systemui.statusbar.CommandQueue;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.PluginDependencyProvider;
 import com.android.systemui.recents.Recents;
-import com.android.systemui.statusbar.phone.dagger.StatusBarComponent;
+import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.shared.plugins.PluginManager;
 import com.android.systemui.stackdivider.Divider;
-import com.android.systemui.statusbar.phone.LightsOutNotifController;
-import com.android.systemui.statusbar.phone.StatusBarNotificationActivityStarter;
-import com.android.systemui.statusbar.phone.ShadeController;
-import com.android.systemui.statusbar.SuperStatusBarViewFactory;
-import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
-import com.android.keyguard.ViewMediatorCallback;
-import com.android.systemui.InitController;
-import com.android.systemui.plugins.DarkIconDispatcher;
-import com.android.systemui.plugins.PluginDependencyProvider;
-import com.android.systemui.statusbar.phone.KeyguardDismissUtil;
-import com.android.systemui.statusbar.policy.ExtensionController;
-import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
-import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy;
-import com.android.systemui.statusbar.phone.StatusBar;
+import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.KeyguardIndicationController;
-import com.android.systemui.keyguard.DismissCallbackRegistry;
+import com.android.systemui.statusbar.NavigationBarController;
+import com.android.systemui.statusbar.NotificationLockscreenUserManager;
+import com.android.systemui.statusbar.NotificationMediaManager;
+import com.android.systemui.statusbar.NotificationRemoteInputManager;
 import com.android.systemui.statusbar.NotificationShadeDepthController;
+import com.android.systemui.statusbar.NotificationViewHierarchyManager;
+import com.android.systemui.statusbar.PulseExpansionHandler;
+import com.android.systemui.statusbar.SuperStatusBarViewFactory;
+import com.android.systemui.statusbar.SysuiStatusBarStateController;
+import com.android.systemui.statusbar.VibratorHelper;
+import com.android.systemui.statusbar.phone.dagger.StatusBarComponent;
+import com.android.systemui.statusbar.notification.DynamicPrivacyController;
+import com.android.systemui.statusbar.notification.NotificationWakeUpCoordinator;
+import com.android.systemui.statusbar.notification.VisualStabilityManager;
+import com.android.systemui.statusbar.notification.init.NotificationsController;
+import com.android.systemui.statusbar.notification.interruption.BypassHeadsUpNotifier;
+import com.android.systemui.statusbar.notification.interruption.NotificationInterruptStateProvider;
+import com.android.systemui.statusbar.notification.logging.NotificationLogger;
+import com.android.systemui.statusbar.notification.row.NotificationGutsManager;
+import com.android.systemui.statusbar.phone.AutoHideController;
+import com.android.systemui.statusbar.phone.BiometricUnlockController;
+import com.android.systemui.statusbar.phone.DozeParameters;
+import com.android.systemui.statusbar.phone.DozeScrimController;
+import com.android.systemui.statusbar.phone.DozeServiceHost;
+import com.android.systemui.statusbar.phone.HeadsUpManagerPhone;
+import com.android.systemui.statusbar.phone.KeyguardBypassController;
+import com.android.systemui.statusbar.phone.KeyguardDismissUtil;
+import com.android.systemui.statusbar.phone.KeyguardLiftController;
+import com.android.systemui.statusbar.phone.LightBarController;
+import com.android.systemui.statusbar.phone.LightsOutNotifController;
+import com.android.systemui.statusbar.phone.LockscreenLockIconController;
+import com.android.systemui.statusbar.phone.LockscreenWallpaper;
+import com.android.systemui.statusbar.phone.NotificationGroupManager;
+import com.android.systemui.statusbar.phone.NotificationShadeWindowController;
+import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy;
+import com.android.systemui.statusbar.phone.ShadeController;
+import com.android.systemui.statusbar.phone.StatusBar;
+import com.android.systemui.statusbar.phone.StatusBarIconController;
+import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
+import com.android.systemui.statusbar.phone.StatusBarNotificationActivityStarter;
 import com.android.systemui.statusbar.phone.StatusBarTouchableRegionManager;
+import com.android.systemui.statusbar.phone.dagger.StatusBarComponent;
+import com.android.systemui.statusbar.policy.BatteryController;
+import com.android.systemui.statusbar.policy.ConfigurationController;
+import com.android.systemui.statusbar.policy.DeviceProvisionedController;
+import com.android.systemui.statusbar.policy.ExtensionController;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
+import com.android.systemui.statusbar.policy.NetworkController;
+import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
+import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
+import com.android.systemui.statusbar.policy.UserSwitcherController;
+import com.android.systemui.volume.VolumeComponent;
 
 import com.google.android.systemui.LiveWallpaperScrimController;
+import com.google.android.systemui.NotificationLockscreenUserManagerGoogle;
+import com.google.android.systemui.smartspace.SmartSpaceController;
 
 import dagger.Lazy;
 
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 import java.util.Optional;
 import java.util.concurrent.Executor;
 
@@ -94,7 +106,11 @@ import javax.inject.Inject;
 
 public class StatusBarGoogle extends StatusBar {
 
+    @Inject
+    public SmartSpaceController mSmartSpaceController;
+
     public StatusBarGoogle(
+            SmartSpaceController smartSpaceController,
             Context context,
             NotificationsController notificationsController,
             LightBarController lightBarController,
@@ -250,15 +266,24 @@ public class StatusBarGoogle extends StatusBar {
                 dismissCallbackRegistry,
                 notificationShadeDepthControllerLazy,
                 statusBarTouchableRegionManager);
+        mSmartSpaceController = smartSpaceController;
     }
 
     @Override
     public void start() {
         super.start();
+        ((NotificationLockscreenUserManagerGoogle) Dependency.get(NotificationLockscreenUserManager.class)).updateSmartSpaceVisibilitySettings();
     }
 
     @Override
     public void setLockscreenUser(int i) {
         super.setLockscreenUser(i);
+        mSmartSpaceController.reloadData();
+    }
+
+    @Override
+    public void dump(FileDescriptor fileDescriptor, PrintWriter printWriter, String[] strArr) {
+        super.dump(fileDescriptor, printWriter, strArr);
+        mSmartSpaceController.dump(fileDescriptor, printWriter, strArr);
     }
 }
