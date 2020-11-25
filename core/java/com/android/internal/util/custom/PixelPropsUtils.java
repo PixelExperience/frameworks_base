7eaa359e9fd65bbe4a0c292043ebb5caf1066f1e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-25 01:02:30 -0300

    PixelPropsUtils: Cleanup apps
    Change-Id: Ida149c01cb7bf09a4275e7e436ce99ffc94a5bf0

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -31,29 +31,10 @@ public class PixelPropsUtils {
     private static final Map<String, Object> propsToChange;
 
     private static final String[] packagesToChange = {
-            "com.google.android.ext.services",
-            "com.google.android.apps.pixelmigrate",
             "com.google.android.apps.safetyhub",
-            "com.google.android.as",
-            "com.google.android.dialer",
-            "com.google.intelligence.sense",
-            "com.android.vending",
-            "com.google.android.apps.gcs",
             "com.google.android.apps.turbo",
-            "com.google.android.apps.wellbeing",
-            "com.google.android.configupdater",
-            "com.google.android.gms",
-            "com.google.android.googlequicksearchbox",
-            "com.google.android.settings.intelligence",
-            "com.google.android.setupwizard",
-            "com.google.android.apps.nexuslauncher",
-            "com.google.android.gsf",
-            "com.google.android.apps.wallpaper",
-            "com.google.android.onetimeinitializer",
-            "com.google.android.pixel.setupwizard",
-            "com.google.android.apps.messaging",
-            "com.google.android.apps.photos",
-            "com.google.android.apps.maps"
+            "com.google.android.apps.maps",
+            "com.google.android.gms"
     };
 
     static {
