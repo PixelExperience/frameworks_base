bb8ffcc1c7ce771b306f0b9d92b3d9e920db9c39
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-27 23:00:01 +0000

    FODCircleView: detect touch outside and take action accordingly
    FOD's view was intercepting gestures, we need to avoid that.
    
    Tests: - enable gesture navigation, open a third party-app that asks for biometric, swipe to recents (or home), ensure FOD doesn't intercept touch and swipe action reaches a successful ending.
    Change-Id: Iaae1a373405735bee5526fd6c05228eddeee5211

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -87,6 +87,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private boolean mIsCircleShowing;
     private boolean mIsDreaming;
     private boolean mIsKeyguard;
+    private boolean mTouchedOutside;
 
     private boolean mDozeEnabled;
     private boolean mFodGestureEnable;
@@ -315,11 +316,13 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mParams.packageName = "android";
         mParams.type = WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
         mPressedParams.copyFrom(mParams);
-        mPressedParams.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        mPressedParams.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND |
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
 
         mParams.setTitle("Fingerprint on display");
         mPressedParams.setTitle("Fingerprint on display.touched");
@@ -369,6 +372,12 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         float y = event.getAxisValue(MotionEvent.AXIS_Y);
 
         boolean newIsInside = (x > 0 && x < mSize) && (y > 0 && y < mSize);
+        mTouchedOutside = false;
+
+        if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+            mTouchedOutside = true;
+            return true;
+        }
 
         if (event.getAction() == MotionEvent.ACTION_DOWN && newIsInside) {
             showCircle();
@@ -443,7 +452,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     }
 
     public void showCircle() {
-        if (mFading) return;
+        if (mFading || mTouchedOutside) return;
         mIsCircleShowing = true;
 
         setKeepScreenOn(true);
