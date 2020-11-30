18a7e87bac9a5b8662c7212be198b958e4a75f38
Chenyang Zhong <zhongcy95@gmail.com>
2020-11-29 21:03:31 -0500

    PixelPropsUtils: bring back wallpaper collection
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Ia3c11cc377107ed856fc9d9594aad45bf5a49185

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -33,6 +33,7 @@ public class PixelPropsUtils {
     private static final String[] packagesToChange = {
             "com.google.android.apps.safetyhub",
             "com.google.android.apps.turbo",
+            "com.google.android.apps.wallpaper",
             "com.google.android.apps.maps",
             "com.google.android.gms"
     };
