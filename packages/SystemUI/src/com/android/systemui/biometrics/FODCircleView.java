c3807e607a6f318681a7fde5b7fb9726011d230d
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-30 04:31:41 +0000

    SystemUI: Don't show FOD icon/pad keyguard if strong auth is required
    Change-Id: Ie227aee88fea4945a7618fe606c1094ae812ebe0

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -93,6 +93,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             new IFingerprintInscreenCallback.Stub() {
         @Override
         public void onFingerDown() {
+            if (mUpdateMonitor.userNeedsStrongAuth()) {
+                // Keyguard requires strong authentication (not biometrics)
+                return;
+            }
+
             mHandler.post(() -> showCircle());
         }
 
@@ -387,6 +392,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     }
 
     public void show() {
+        if (mUpdateMonitor.userNeedsStrongAuth()) {
+            // Keyguard requires strong authentication (not biometrics)
+            return;
+        }
+
         if (!mUpdateMonitor.isScreenOn()) {
             // Keyguard is shown just after screen turning off
             return;
