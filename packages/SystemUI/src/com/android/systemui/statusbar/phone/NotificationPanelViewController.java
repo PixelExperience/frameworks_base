7a6e4f76a35db8922ab80aa5a8596ccc9c9fea8d
Arian <arian.kulmer@web.de>
2020-12-27 14:35:33 +0100

    NotificationPanelViewController: Fix DT2S gesture handling
    * Checking if mBarState equals keyguard here causes an issue so that
      three taps are required until the screen turns off.
    * The check does not seem to be needed here, hence remove it to fix
      this issue.
    
    Behavior without this patch: https://imgur.com/a/EISQDAF
    Behavior with this patch: https://imgur.com/a/vsQHveD
    
    Change-Id: I9f2d3c63d397c998bea5137f747a9ad95ae2c746

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4245,7 +4245,7 @@ public class NotificationPanelViewController extends PanelViewController {
                     return false;
                 }
 
-                if (mDoubleTapToSleepEnabled && mBarState == StatusBarState.KEYGUARD) {
+                if (mDoubleTapToSleepEnabled && !mPulsing && !mDozing) {
                     mDoubleTapGesture.onTouchEvent(event);
                 }
 
