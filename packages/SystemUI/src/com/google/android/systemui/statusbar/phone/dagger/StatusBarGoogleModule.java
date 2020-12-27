fac17aafa663b41d8236588f01f78ee7680af303
ezio84 <brabus84@gmail.com>
2020-12-27 23:52:54 +0100

    SystemUIGoogle port: don't override aosp scrim controller
    redfin livewallpapers scrim controller breaks AoD
    to replicate the issue:
    - enable AoD (without lift to wake)
    - cover the proximity sensor and wait till AoD gets paused and screen
      goes off
    - uncover the sensor
    - screen is still black (it's not really OFF, indeed AoD gets
      started correctly and screen becomes active - dozeservice debugging
    can show this - )
    
    Change-Id: I341d58984d51dbe4e94f333c809ec0a29e46e6f5

--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/dagger/StatusBarGoogleModule.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/dagger/StatusBarGoogleModule.java
@@ -102,7 +102,6 @@ import com.android.systemui.volume.VolumeComponent;
 
 import com.android.systemui.statusbar.phone.StatusBar;
 
-import com.google.android.systemui.LiveWallpaperScrimController;
 import com.google.android.systemui.statusbar.phone.StatusBarGoogle;
 import com.google.android.systemui.statusbar.phone.WallpaperNotifier;
 import com.google.android.systemui.smartspace.SmartSpaceController;
@@ -170,7 +169,7 @@ public interface StatusBarGoogleModule {
             NotificationShadeWindowController notificationShadeWindowController,
             LockscreenLockIconController lockscreenLockIconController,
             DozeParameters dozeParameters,
-            LiveWallpaperScrimController liveWallpaperScrimController,
+            ScrimController scrimController,
             @Nullable KeyguardLiftController keyguardLiftController,
             Lazy<LockscreenWallpaper> lockscreenWallpaperLazy,
             Lazy<BiometricUnlockController> biometricUnlockControllerLazy,
@@ -251,7 +250,7 @@ public interface StatusBarGoogleModule {
                 notificationShadeWindowController,
                 lockscreenLockIconController,
                 dozeParameters,
-                liveWallpaperScrimController,
+                scrimController,
                 keyguardLiftController,
                 lockscreenWallpaperLazy,
                 biometricUnlockControllerLazy,
