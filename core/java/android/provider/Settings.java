a58c03d82e77783cab70d2c019c26a7810d4eb14
Arian <arian.kulmer@web.de>
2021-01-03 16:49:50 +0100

    LineageHardwareManager: Introduce keys swap feature support
    @jhenrique09 edits: Adapt to AOSP
    
    Change-Id: I789814d780b06b146c1f9ab62293f4d6ca287208

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11339,6 +11339,12 @@ public final class Settings {
         public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
 
         /**
+         * Whether key swap is enabled on supported hardware
+         * @hide
+         */
+        public static final String SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
