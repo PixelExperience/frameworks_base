948df1f5a38e9cbc1ae9b2b98bdf221e4314d3a7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-01-27 19:23:39 -0300

    Add screenshot action for hwkeys
    Change-Id: Iacb101c3cec7091a0e34cc5514bddfe3c7ea84a5

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1945,6 +1945,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case SPLIT_SCREEN:
                 toggleSplitScreen();
                 break;
+            case SCREENSHOT:
+                interceptScreenshotChord(SCREENSHOT_KEY_OTHER, 0 /*pressDelay*/);
+                break;
             default:
                 break;
         }
