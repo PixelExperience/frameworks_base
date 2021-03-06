c8fae1b9b5507cb4f6a3c1ae16718f7304795422
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-03-06 15:27:11 -0300

    [1/2] Allow using face as auth method for apps
    Change-Id: I008370cefe9584c86e058b718e2ef79518b95eb6

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthBiometricView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthBiometricView.java
@@ -104,6 +104,7 @@ public abstract class AuthBiometricView extends LinearLayout {
         int ACTION_BUTTON_TRY_AGAIN = 4;
         int ACTION_ERROR = 5;
         int ACTION_USE_DEVICE_CREDENTIAL = 6;
+        int ACTION_USE_FACE = 7;
 
         /**
          * When an action has occurred. The caller will only invoke this when the callback should
@@ -127,6 +128,10 @@ public abstract class AuthBiometricView extends LinearLayout {
 
         public Button getTryAgainButton() {
             return mBiometricView.findViewById(R.id.button_try_again);
+        }
+
+        public Button getUseFaceButton() {
+            return mBiometricView.findViewById(R.id.button_use_face);
         }
 
         public TextView getTitleView() {
@@ -179,6 +184,7 @@ public abstract class AuthBiometricView extends LinearLayout {
     @VisibleForTesting Button mNegativeButton;
     @VisibleForTesting Button mPositiveButton;
     @VisibleForTesting Button mTryAgainButton;
+    Button mUseFaceButton;
 
     // Measurements when biometric view is showing text, buttons, etc.
     private int mMediumHeight;
@@ -605,6 +611,7 @@ public abstract class AuthBiometricView extends LinearLayout {
         mNegativeButton = mInjector.getNegativeButton();
         mPositiveButton = mInjector.getPositiveButton();
         mTryAgainButton = mInjector.getTryAgainButton();
+        mUseFaceButton = mInjector.getUseFaceButton();
 
         mNegativeButton.setOnClickListener((view) -> {
             if (mState == STATE_PENDING_CONFIRMATION) {
@@ -629,7 +636,14 @@ public abstract class AuthBiometricView extends LinearLayout {
             Utils.notifyAccessibilityContentChanged(mAccessibilityManager, this);
         });
 
+        mUseFaceButton.setOnClickListener((view) -> {
+            mCallback.onAction(Callback.ACTION_USE_FACE);
+        });
+
         if (this instanceof AuthBiometricFingerprintView) {
+            if (!Utils.canAuthenticateWithFace(mContext, mUserId)){
+                mUseFaceButton.setVisibility(View.GONE);
+            }
             if (mHasFod) {
                 boolean isGesturalNav = Integer.parseInt(Settings.Secure.getStringForUser(
                         mContext.getContentResolver(), Settings.Secure.NAVIGATION_MODE,
@@ -654,6 +668,7 @@ public abstract class AuthBiometricView extends LinearLayout {
             }
         } else if (this instanceof AuthBiometricFaceView) {
             mIconView.setVisibility(View.VISIBLE);
+            mUseFaceButton.setVisibility(View.GONE);
         }
     }
 
