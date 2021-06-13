2bb3e2b79119331a9f92986f0891233d03e31c80
jhenrique09 <jhsv09@gmail.com>
2021-06-13 19:06:48 -0300

    GlobalActions: Drop bugreport action
    Change-Id: I49bc3a0551d0769df72665460d7c9da982e15c22

--- core/java/com/android/internal/util/custom/globalactions/PowerMenuConstants.java
+++ core/java/com/android/internal/util/custom/globalactions/PowerMenuConstants.java
@@ -26,7 +26,6 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_USERS = "users";
     public static final String GLOBAL_ACTION_KEY_SETTINGS = "settings";
     public static final String GLOBAL_ACTION_KEY_LOCKDOWN = "lockdown";
-    public static final String GLOBAL_ACTION_KEY_BUGREPORT = "bugreport";
     public static final String GLOBAL_ACTION_KEY_SILENT = "silent";
     public static final String GLOBAL_ACTION_KEY_VOICEASSIST = "voiceassist";
     public static final String GLOBAL_ACTION_KEY_ASSIST = "assist";
@@ -51,7 +50,6 @@ public class PowerMenuConstants {
         GLOBAL_ACTION_KEY_AIRPLANE,
         GLOBAL_ACTION_KEY_USERS,
         GLOBAL_ACTION_KEY_SETTINGS,
-        GLOBAL_ACTION_KEY_BUGREPORT,
         GLOBAL_ACTION_KEY_SILENT,
         GLOBAL_ACTION_KEY_VOICEASSIST,
         GLOBAL_ACTION_KEY_ASSIST,
