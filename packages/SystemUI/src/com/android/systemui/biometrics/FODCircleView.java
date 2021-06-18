6e40bd228feb5fdb39b59fa88a7a81ef84f107cb
jhenrique09 <jhsv09@gmail.com>
2021-06-18 11:57:48 -0300

    fixup - [squash]FODCircleView: remove fod dim and inline with los
    Change-Id: Ibfd73113662b0fe9ff0b7c39131df0b924e7af17
    (cherry picked from commit aed511b50a13f9b6273ea9ce908f43919ec923b8)

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -203,11 +203,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         }
 
         @Override
-        public void onStartedGoingToSleep(int why) {
-            hide();
-        }
-
-        @Override
         public void onScreenTurnedOff() {
             mScreenTurnedOn = false;
             if (mFodGestureEnable){
