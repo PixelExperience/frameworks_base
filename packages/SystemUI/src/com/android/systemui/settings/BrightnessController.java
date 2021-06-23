3e8f2b97d65ace27a4bd9f7492ba1203f1258f6b
Chenyang Zhong <zhongcy95@gmail.com>
2021-06-23 19:11:24 -0400

    BrightnessController: tune the slider animation duration
    The brightness slider is animated at a fixed max duration of
    3000ms. If the brightness ramp rate is reduced to a value that is
    much lower than the default rate, the brightness slider animation
    can be considerably out of sync with the screen brightness
    transitions.
    
    Override the slider animator duration scale by the ratio of the
    default ramp rate to the actual rate in use. The maximum scale is
    clamped to 3x. Note that the purpose is not to make the slider
    perfectly in sync with the screen, as the original design was
    not like that either.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I980a8976d1d8e6fe9c295be9a06ee305a66c9448

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -23,6 +23,7 @@ import static com.android.settingslib.display.BrightnessUtils.convertLinearToGam
 import android.animation.ValueAnimator;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.hardware.display.DisplayManager;
 import android.net.Uri;
@@ -96,6 +97,8 @@ public class BrightnessController implements ToggleSlider.Listener {
     private boolean mControlValueInitialized;
 
     private ValueAnimator mSliderAnimator;
+    private static final float mDefaultBrightnessRampRateSlow = 0.2352941f;
+    private final float mBrightnessRampRateSlow;
 
     public interface BrightnessStateChangeCallback {
         public void onBrightnessLevelChanged();
@@ -327,6 +330,10 @@ public class BrightnessController implements ToggleSlider.Listener {
         mDisplayManager = context.getSystemService(DisplayManager.class);
         mVrManager = IVrManager.Stub.asInterface(ServiceManager.getService(
                 Context.VR_SERVICE));
+
+        final Resources resources = context.getResources();
+        mBrightnessRampRateSlow = resources.getFloat(
+                com.android.internal.R.dimen.config_brightnessRampRateSlowFloat);
     }
 
     public void addStateChangedCallback(BrightnessStateChangeCallback cb) {
@@ -472,6 +479,13 @@ public class BrightnessController implements ToggleSlider.Listener {
         final long animationDuration = SLIDER_ANIMATION_DURATION * Math.abs(
                 mControl.getValue() - target) / GAMMA_SPACE_MAX;
         mSliderAnimator.setDuration(animationDuration);
+        // Only override the duration scale when the ramp rate is different from the default value
+        if (mBrightnessRampRateSlow > 0 &&
+                    Math.abs(mBrightnessRampRateSlow - mDefaultBrightnessRampRateSlow) > 0.01f) {
+            float durationScale = Math.min(
+                    mDefaultBrightnessRampRateSlow / mBrightnessRampRateSlow, 3.0f);
+            mSliderAnimator.overrideDurationScale(durationScale);
+        }
         mSliderAnimator.start();
     }
 
