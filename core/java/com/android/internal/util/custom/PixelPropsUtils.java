06e16283f763cc56bb7306b2c7a66799bb8d1bbe
LordShenron <shen.priyanshu@gmail.com>
2021-08-30 09:16:27 +0200

    PixelPropsUtils: add galaxy watch 4 plugin to spoof list
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -66,7 +66,8 @@ public class PixelPropsUtils {
             "com.samsung.android.geargplugin",
             "com.samsung.android.gearnplugin",
             "com.samsung.android.modenplugin",
-            "com.samsung.android.neatplugin"
+            "com.samsung.android.neatplugin",
+            "com.samsung.android.waterplugin"
     };
 
     private static final String[] packagesToChangePixel2 = {
