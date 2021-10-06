cbe23053aa098d661c5b91c9c01ceba1aab39270
TheScarastic <warabhishek@gmail.com>
2021-10-06 06:51:36 +0000

    udfps: Allow to configure hbm overlay type
     * Local HBM does not shows pressed icon
     * Global hbm shows pressed icon
    
    Change-Id: Ic9f51a33c1781527f0c7fc41e8bfe320bf2653d2

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.java
@@ -50,7 +50,6 @@ public class UdfpsView extends FrameLayout implements DozeReceiver, UdfpsIllumin
 
     private static final String SETTING_HBM_TYPE =
             "com.android.systemui.biometrics.UdfpsSurfaceView.hbmType";
-    private static @HbmType int DEFAULT_HBM_TYPE;
 
     private static final int DEBUG_TEXT_SIZE_PX = 32;
 
@@ -99,9 +98,11 @@ public class UdfpsView extends FrameLayout implements DozeReceiver, UdfpsIllumin
         DEFAULT_HBM_TYPE = mContext.getResources().getInteger(R.integer.config_udfps_hbm_type);
         if (Build.IS_ENG || Build.IS_USERDEBUG) {
             mHbmType = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                    SETTING_HBM_TYPE, DEFAULT_HBM_TYPE, UserHandle.USER_CURRENT);
+                    SETTING_HBM_TYPE,
+                    mContext.getResources().getInteger(R.integer.config_udfpsHbmType),
+                    UserHandle.USER_CURRENT);
         } else {
-            mHbmType = DEFAULT_HBM_TYPE;
+            mHbmType = mContext.getResources().getInteger(R.integer.config_udfpsHbmType);
         }
     }
 
