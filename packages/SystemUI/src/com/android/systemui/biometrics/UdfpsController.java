dd9fb9981d15468c0bd5347fec89886aa52db289
TheScarastic <warabhishek@gmail.com>
2021-10-06 06:53:09 +0000

    udfps: Add support for udfps on aod without having dedicated sensor
    Change-Id: I4aa39efe3714bcc40ab3da862809ca9c8974c6bd

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -320,6 +320,19 @@ public class UdfpsController implements DozeReceiver {
                 mView.setDebugMessage(message);
             });
         }
+
+        @Override
+        public void onAcquired(int sensorId, int acquiredInfo, int vendorCode) {
+            mFgExecutor.execute(() -> {
+                if (acquiredInfo == 6 && (mStatusBarStateController.isDozing() || !mScreenOn)) {
+                    if (vendorCode == 22) { // Use overlay to determine pressed vendor code?
+                        mPowerManager.wakeUp(mSystemClock.uptimeMillis(),
+                                PowerManager.WAKE_REASON_GESTURE, TAG);
+                        onAodInterrupt(0, 0, 0, 0); // To-Do pass proper values
+                    }
+                }
+            });
+        }
     }
 
     /**
