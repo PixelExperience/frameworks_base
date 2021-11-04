6ce39fb8603ce6d1d375b6128606aa38295e63aa
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-11-05 01:52:51 +0300

    [SQUASHED] core: Blacklist pixel system feature from Google Photos
        We want to include the P21 experience flag to enable new features,
        however it seems like Google Photos uses it to decide whether to use the
        TPU tflite delegate. There doesn't seem to be any fallback so we need to
        make sure the feature is not exposed to the app so that a normal
        NNAPI/GPU delegate can be used instead.
    
        Test: Google Photos editor with PIXEL_2021_EXPERIENCE feature in product
        Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
        Change-Id: I51a02f8347324c7a85f3136b802dce4cc4556ac5
    
    commit 67eb31b3bb43d06fcc7f6fdb2f92eb486451cae6
    Author: kondors1995 <normandija1945@gmail.com>
    Date:   Thu Jun 9 17:39:25 2022 +0530
    
        Core: Extend Pixel experience Blacklist For Google Photos
    
        Turns out having these brakes Original quality backups.
        Since these indicate that the device is pixel 4 with in the turn brakes device spoofing as OG pixel
    
        Change-Id: I336facff7b55552f094997ade337656461a0ea1d
    
    commit 508a99cde60b73dc3f1e843d569bca31def35988
    Author: ReallySnow <reallysnow233@gmail.com>
    Date:   Fri Dec 31 16:40:23 2021 +0800
    
        base: core: Blacklist Pixel 2017 and 2018 exclusive for Google Photos
    
        * In this way can use PixelPropsUtils to simulate the Pixel XL prop
          method to use the unlimited storage space of Google Photos
        * Thanks nullbytepl for the idea
    
        Change-Id: I92d472d319373d648365c8c63e301f1a915f8de9
    
    commit aaf07f6ccc89c2747b97bc6dc2ee4cb7bd2c6727
    Author: Akash Srivastava <akashniki@gmail.com>
    Date:   Sat Aug 20 19:04:32 2022 +0700
    
        core: Pixel experience Blacklist For Google Photos for Android 13
    
        * See, in Android 13 pixel_experience_2022_midyear was added, which needs to be blacklisted aswell
    
        Change-Id: Id36d12afeda3cf6b39d01a0dbe7e3e9058659b8e
    
    commit 9d6e5749a988c9051b1d47c11bb02daa7b1b36fd
    Author: spezi77 <spezi7713@gmx.net>
    Date:   Mon Jan 31 19:17:34 2022 +0100
    
        core: Rework the ph0t0s features blacklist
    
        * Moving the flags to an array feels more like a blacklist :P
        * Converted the flags into fully qualified package names, while at it
    
        Signed-off-by: spezi77 <spezi7713@gmx.net>
        Change-Id: I4b9e925fc0b8c01204564e18b9e9ee4c7d31c123
    
    commit d7201c0cff326a6374e29aa79c6ce18828f96dc6
    Author: Joey Huab <joey@evolution-x.org>
    Date:   Tue Feb 15 17:32:11 2022 +0900
    
        core: Refactor Pixel features
    
        * Magic Eraser is wonky and hard to
          enable and all this mess isn't really worth
          the trouble so just stick to the older setup.
    
        * Default Pixel 5 spoof for Photos and only switch
          to Pixel XL when spoof is toggled.
    
        * We will try to bypass 2021 features and Raven
          props for non-Pixel 2021 devices as apps usage
          requires TPU.
    
        * Remove P21 experience system feature check
    
    Change-Id: Iffae2ac87ce5428daaf6711414b86212814db7f2

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -806,8 +806,51 @@ public class ApplicationPackageManager extends PackageManager {
                 }
             };
 
+    private static final String[] featuresPixel = {
+            "com.google.android.apps.photos.PIXEL_2019_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2018_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2017_PRELOAD",
+            "com.google.android.feature.PIXEL_2020_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2019_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2019_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2018_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2017_EXPERIENCE",
+            "com.google.android.feature.PIXEL_EXPERIENCE",
+            "com.google.android.feature.GOOGLE_BUILD",
+            "com.google.android.feature.GOOGLE_EXPERIENCE"
+    };
+
+    private static final String[] featuresP22 = {
+            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+    };
+
+    private static final String[] featuresP21 = {
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE"
+    };
+
+    private static final String[] featuresNexus = {
+            "com.google.android.apps.photos.NEXUS_PRELOAD",
+            "com.google.android.apps.photos.nexus_preload",
+            "com.google.android.feature.PIXEL_EXPERIENCE",
+            "com.google.android.feature.GOOGLE_BUILD",
+            "com.google.android.feature.GOOGLE_EXPERIENCE"
+    };
+
     @Override
     public boolean hasSystemFeature(String name, int version) {
+        String packageName = ActivityThread.currentPackageName();
+        if (packageName != null &&
+                packageName.equals("com.google.android.apps.photos")) {
+            if (Arrays.asList(featuresPixel).contains(name)) return false;
+            if (Arrays.asList(featuresP22).contains(name)) return false;
+            if (Arrays.asList(featuresP21).contains(name)) return false;
+            if (Arrays.asList(featuresNexus).contains(name)) return true;
+        }
+        if (Arrays.asList(featuresPixel).contains(name)) return true;
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
     }
 
