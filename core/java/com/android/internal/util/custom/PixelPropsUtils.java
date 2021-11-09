b1b6de78d8c0b9139fb8115ef2368d6f81d1f5bf
jhenrique09 <jhsv09@gmail.com>
2021-11-09 10:01:05 -0300

    PixelPropsUtils: Use Build.VERSION.INCREMENTAL
    Change-Id: If70b940607e0cc1c8cd7c0312c983ab5b5e65216

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -74,7 +74,7 @@ public class PixelPropsUtils {
         }
         // Set proper indexing fingerprint
         if (packageName.equals("com.google.android.settings.intelligence")){
-            setPropValue("FINGERPRINT", Build.DATE);
+            setPropValue("FINGERPRINT", Build.VERSION.INCREMENTAL);
         }
     }
 
