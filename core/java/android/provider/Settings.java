abdf67ed64b93f29dcec2bc596a46001054e4724
jhonboy121 <alfredmathew05@gmail.com>
2021-11-14 13:23:32 +0530

    base: Allow toggling screen off FOD [1/2]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I1feece846e4dbddb276ad6ed0efac77e5b7986d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10958,6 +10958,14 @@ public final class Settings {
          */
         public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
+         /**
+         * Enable udfps detection even when screen is off
+         * Default value is 0
+         * @hide
+         */
+        @Readable
+        public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
