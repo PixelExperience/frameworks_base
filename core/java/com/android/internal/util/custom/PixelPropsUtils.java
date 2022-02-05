5017b2d530d5712770bd851cada56c368b5ef274
Karan Parashar <whyredfire@gmail.com>
2022-02-05 08:01:40 +0530

    PixelPropsUtils: Avoid spoofing for Pixel Buds
    - it breaks the app, thanks to @ helenius147 [TG] for pointing this out
    
    Change-Id: Id0dd0af78ca78db6ad50617e04dbc880c0515c2e
    (cherry picked from commit d19d773bf1aa2a1c5abcc15e72ab1347c6e2f5c7)

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -72,7 +72,8 @@ public class PixelPropsUtils {
         "com.google.android.MTCL83",
         "com.google.android.UltraCVM",
         "com.google.android.apps.cameralite",
-        "com.google.ar.core"
+        "com.google.ar.core",
+        "com.google.android.apps.wearables.maestro.companion"
     };
 
     // Codenames for currently supported Pixels by Google
