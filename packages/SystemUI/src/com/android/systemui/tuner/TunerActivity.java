abb7cab854f5bd806d9a00052e41214f35b47042
SagarMakhar <sagarmakhar@gmail.com>
2022-03-10 15:23:12 -0300

    base: SystemUI: Adapt tuner prefs to S settings style
    Change-Id: Ib02c0166729c7bf828698323dac41fe3a14cf8a8

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -36,7 +36,9 @@ import com.android.systemui.fragments.FragmentService;
 
 import javax.inject.Inject;
 
-public class TunerActivity extends Activity implements
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+public class TunerActivity extends CollapsingToolbarBaseActivity implements
         PreferenceFragment.OnPreferenceStartFragmentCallback,
         PreferenceFragment.OnPreferenceStartScreenCallback {
 
@@ -53,16 +55,12 @@ public class TunerActivity extends Activity implements
     }
 
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_AppCompat_DayNight);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.tuner_activity);
-        Toolbar toolbar = findViewById(R.id.action_bar);
-        if (toolbar != null) {
-            setActionBar(toolbar);
-        }
 
         if (getFragmentManager().findFragmentByTag(TAG_TUNER) == null) {
             final String action = getIntent().getAction();
