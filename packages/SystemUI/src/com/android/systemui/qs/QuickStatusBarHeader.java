f210a2f5500465cb2e140d63c30992dc9072aa99
jhenrique09 <jhsv09@gmail.com>
2022-04-06 16:12:13 +0000

    NetworkTraffic: Allow to disable on statusbar with overlay [1/2]
    Some devices doesn't have enough space on statusbar due to big notchs, add a overlay to force disable network traffic on statusbar for better user UX
    
    Change-Id: Id78efab2375b3b5944d4a4fd2535d420c3f80689

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -115,6 +115,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private NetworkTraffic mNetworkTraffic;
     private boolean mShowNetworkTraffic;
 
+    private boolean mSupportsNetworkTrafficOnStatusBar;
+
     public QuickStatusBarHeader(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -154,6 +156,9 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
 
         mNetworkTraffic = findViewById(R.id.network_traffic);
+
+        mSupportsNetworkTrafficOnStatusBar = mContext.getResources().getBoolean(
+            com.android.internal.R.bool.config_supportsNetworkTrafficOnStatusBar);
 
         updateResources();
 
@@ -588,8 +593,9 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                 mContext, newValue).contains("clock"));
         switch (key) {
             case NETWORK_TRAFFIC_LOCATION:
-                mShowNetworkTraffic =
-                        TunerService.parseInteger(newValue, 0) == 2;
+                int networkTrafficState = TunerService.parseInteger(newValue, 0);
+                mShowNetworkTraffic = (networkTrafficState == 2 ||
+                    (networkTrafficState == 1 && !mSupportsNetworkTrafficOnStatusBar));
                 setChipVisibility(mPrivacyChip.getVisibility() == View.VISIBLE);
                 break;
             default:
