152728ebb78bac0b42fb5c8149513e9891afedb6
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-27 16:54:20 +0300

    SystemUI: doze: add config for long press sensors that need a proximity check
    Change-Id: I34d5a8313304e3ac8bd53282de66ab1d3d1653a8

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -226,7 +226,8 @@ public class DozeSensors {
                         true /* touchscreen */,
                         false /* ignoresSetting */,
                         dozeParameters.longPressUsesProx() /* requiresProx */,
-                        true /* immediatelyReRegister */),
+                        true /* immediatelyReRegister */,
+                        !dozeParameters.longPressNeedsProximityCheck() /* performsProxCheck */),
                 new TriggerSensor(
                         findSensor(config.udfpsLongPressSensorType()),
                         "doze_pulse_on_auth",
@@ -237,7 +238,8 @@ public class DozeSensors {
                         true /* touchscreen */,
                         false /* ignoresSetting */,
                         dozeParameters.longPressUsesProx(),
-                        false /* immediatelyReRegister */),
+                        false /* immediatelyReRegister */,
+                        !dozeParameters.longPressNeedsProximityCheck() /* performsProxCheck */),
                 new PluginSensor(
                         new SensorManagerPlugin.Sensor(TYPE_WAKE_DISPLAY),
                         Settings.Secure.DOZE_WAKE_DISPLAY_GESTURE,
