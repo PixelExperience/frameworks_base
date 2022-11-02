a472e90e694e033cb51d70d543180b9e93708d5e
jhenrique09 <jhsv09@gmail.com>
2022-11-02 19:26:35 -0300

    DeviceKeysConstants: Remove split screen actions
    Change-Id: I976143d9eb74fb38fa1d07ab6dd16031d05933e2

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1958,9 +1958,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case SLEEP:
                 mPowerManager.goToSleep(SystemClock.uptimeMillis());
                 break;
-            case SPLIT_SCREEN:
-                toggleSplitScreen();
-                break;
             case SCREENSHOT:
                 interceptScreenshotChord(
                         TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0 /*pressDelay*/);
@@ -4026,13 +4023,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
         if (statusbar != null) {
             statusbar.toggleRecentApps();
-        }
-    }
-
-    private void toggleSplitScreen() {
-        StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
-        if (statusbar != null) {
-            statusbar.toggleSplitScreen();
         }
     }
 
