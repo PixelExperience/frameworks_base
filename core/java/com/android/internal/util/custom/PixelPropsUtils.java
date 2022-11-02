dda0d435887cd7b946abe4837222f25c7e39f62c
aswin7469 <aswinas@pixysos.com>
2022-11-02 15:20:37 +0530

    PixelPropsUtils: spoof nothing smartcenter to pixel
    * after the update to 2.0.2 , its checking for NTExperince on phone 1 (Spacewar) as the custom rom's lacking it just crashes so workaround this by spoofing it
    
    TEST: app opens and successfully connect to ear (1)
    Change-Id: Iad9cf39bb8e55e4c22ff237d8e2ee5160dcea4e1
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -57,7 +57,8 @@ public class PixelPropsUtils {
     private static final String[] extraPackagesToChange = {
             "com.android.chrome",
             "com.android.vending",
-            "com.breel.wallpapers20"
+            "com.breel.wallpapers20",
+            "com.nothing.smartcenter"
     };
 
     private static final String[] packagesToKeep = {
