c9964fad1050445ccbeba840e3479071f82e9895
jhenrique09 <jhsv09@gmail.com>
2023-01-19 23:19:48 -0300

    PixelPropsUtils: Remove spoof for VPN
    It doesn't works
    
    Change-Id: Ide7db38896956eed72e8960e1803be0d6a7357c5

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -45,7 +45,6 @@ public class PixelPropsUtils {
 
     private static final String[] packagesToChangePixel7Pro = {
             "com.google.android.apps.wallpaper",
-            "com.google.android.apps.privacy.wildlife",
             "com.google.android.apps.subscriptions.red"
     };
 
