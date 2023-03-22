cb85d35ada91545cbc61fd17ec95137758d65504
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-22 15:18:20 +0000

    PixelPropsUtils: Spoof Pixel 7 Pro to some Google apps
    This unlocks some non-TPU reliant, useful features across all devices.
    
    @jhenrique09: Update some apps
    
    Change-Id: Ieb09625d5d0d2d5f7036db4ae5a0036b9e4a48fa
    (cherry picked from commit 71232742456969f56723544b2d00e3f36fdb3212)

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -43,13 +43,22 @@ public class PixelPropsUtils {
     private static final Map<String, ArrayList<String>> propsToKeep;
 
     private static final String[] packagesToChangePixel7Pro = {
+            "com.google.android.inputmethod.latin",
+            "com.google.android.setupwizard",
+            "com.google.android.apps.turbo",
+            "com.google.android.googlequicksearchbox",
             "com.google.android.apps.wallpaper",
-            "com.google.android.apps.privacy.wildlife"
+            "com.google.android.apps.wallpaper.pixel",
+            "com.google.android.apps.privacy.wildlife",
+            "com.google.android.apps.googleassistant",
+            "com.google.android.apps.nbu.files",
+            "com.google.android.apps.podcasts",
+            "com.google.android.contacts",
+            "com.google.android.deskclock"
     };
 
     private static final String[] packagesToChangePixelXL = {
-            "com.google.android.apps.photos",
-            "com.google.android.inputmethod.latin"
+            "com.google.android.apps.photos"
     };
 
     private static final String[] extraPackagesToChange = {
@@ -173,6 +182,8 @@ public class PixelPropsUtils {
                     setPropValue("DEVICE", "marlin");
                     setPropValue("MODEL", "Pixel XL");
                     setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
+                }else{
+                    propsToChangePixel7Pro.forEach((k, v) -> setPropValue(k, v));
                 }
                 return;
             }
