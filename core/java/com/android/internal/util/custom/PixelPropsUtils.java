e56bab8d92b8ff8a83bd83423ad39743e73dca67
jhenrique09 <jhsv09@gmail.com>
2023-04-16 19:58:13 -0300

    Revert "PixelPropsUtils: Spoof Pixel 7 Pro to some Google apps"
    This reverts commit cb85d35ada91545cbc61fd17ec95137758d65504.
    
    Change-Id: I52cde6d33ee02eee27247e980acad883229298f9
    (cherry picked from commit 698932064dc77939c0911f1f4cea054e58c27371)

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -44,22 +44,13 @@ public class PixelPropsUtils {
     private static final Map<String, ArrayList<String>> propsToKeep;
 
     private static final String[] packagesToChangePixel7Pro = {
-            "com.google.android.inputmethod.latin",
-            "com.google.android.setupwizard",
-            "com.google.android.apps.turbo",
-            "com.google.android.googlequicksearchbox",
             "com.google.android.apps.wallpaper",
-            "com.google.android.apps.wallpaper.pixel",
-            "com.google.android.apps.privacy.wildlife",
-            "com.google.android.apps.googleassistant",
-            "com.google.android.apps.nbu.files",
-            "com.google.android.apps.podcasts",
-            "com.google.android.contacts",
-            "com.google.android.deskclock"
+            "com.google.android.apps.privacy.wildlife"
     };
 
     private static final String[] packagesToChangePixelXL = {
-            "com.google.android.apps.photos"
+            "com.google.android.apps.photos",
+            "com.google.android.inputmethod.latin"
     };
 
     private static final String[] extraPackagesToChange = {
@@ -191,8 +182,6 @@ public class PixelPropsUtils {
                     setPropValue("DEVICE", "marlin");
                     setPropValue("MODEL", "Pixel XL");
                     setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
-                }else{
-                    propsToChangePixel7Pro.forEach((k, v) -> setPropValue(k, v));
                 }
                 return;
             }
