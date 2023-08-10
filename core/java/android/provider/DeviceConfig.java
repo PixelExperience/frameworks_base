ed9bb0e2d9ec138aa57d483914640f73cf2f1b2c
jhenrique09 <jhsv09@gmail.com>
2023-08-10 14:22:06 +0000

    Implement DeviceConfigUtils and service
    - Allow overriding device configs while using GMS as device configurator package
    
    Change-Id: Ie7327610feca7a390915511088dfef831c946853
    (cherry picked from commit a1148d6552481999cb09d2a6b657fe5fd5f655f9)

--- core/java/android/provider/DeviceConfig.java
+++ core/java/android/provider/DeviceConfig.java
@@ -48,6 +48,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Executor;
 
+import com.android.internal.util.custom.DeviceConfigUtils;
+
 /**
  * Device level configuration parameters which can be tuned by a separate configuration service.
  * Namespaces that end in "_native" such as {@link #NAMESPACE_NETD_NATIVE} are intended to be used
@@ -954,6 +956,9 @@ public final class DeviceConfig {
     @RequiresPermission(WRITE_DEVICE_CONFIG)
     public static boolean setProperty(@NonNull String namespace, @NonNull String name,
             @Nullable String value, boolean makeDefault) {
+        if (DeviceConfigUtils.shouldDenyDeviceConfigControl(namespace, name)){
+            return true;
+        }
         ContentResolver contentResolver = ActivityThread.currentApplication().getContentResolver();
         return Settings.Config.putString(contentResolver, namespace, name, value, makeDefault);
     }
@@ -976,9 +981,15 @@ public final class DeviceConfig {
     @SystemApi
     @RequiresPermission(WRITE_DEVICE_CONFIG)
     public static boolean setProperties(@NonNull Properties properties) throws BadConfigException {
+        Map<String, String> keyValuesFiltered = 
+            DeviceConfigUtils.filterDeviceConfigs(properties.getNamespace(), properties.mMap);
+        if (keyValuesFiltered.size() == 0){
+            return true;
+        }
         ContentResolver contentResolver = ActivityThread.currentApplication().getContentResolver();
-        return Settings.Config.setStrings(contentResolver, properties.getNamespace(),
-                properties.mMap);
+        boolean result = Settings.Config.setStrings(contentResolver, properties.getNamespace(),
+                keyValuesFiltered);
+        return result;
     }
 
     /**
@@ -993,6 +1004,9 @@ public final class DeviceConfig {
     @SystemApi
     @RequiresPermission(WRITE_DEVICE_CONFIG)
     public static boolean deleteProperty(@NonNull String namespace, @NonNull String name) {
+        if (DeviceConfigUtils.shouldDenyDeviceConfigControl(namespace, name)){
+            return true;
+        }
         ContentResolver contentResolver = ActivityThread.currentApplication().getContentResolver();
         return Settings.Config.deleteString(contentResolver, namespace, name);
     }
@@ -1027,6 +1041,7 @@ public final class DeviceConfig {
     public static void resetToDefaults(@ResetMode int resetMode, @Nullable String namespace) {
         ContentResolver contentResolver = ActivityThread.currentApplication().getContentResolver();
         Settings.Config.resetToDefaults(contentResolver, resetMode, namespace);
+        DeviceConfigUtils.setDefaultProperties(contentResolver);
     }
 
     /**
