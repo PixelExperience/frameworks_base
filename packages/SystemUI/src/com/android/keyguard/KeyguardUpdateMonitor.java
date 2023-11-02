0b08405171fc663c021f185e832517fe49591971
Michael Bestas <mkbestas@gmail.com>
2023-11-02 02:22:44 +0200

    SystemUI: Respect single-touch wakeup setting on rear & front fps devices
    Change-Id: I8c279d5c9ccb2298e84a0af18758c3604a70d9d7

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -3154,7 +3154,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 && userDoesNotHaveTrust);
 
         boolean shouldListenSideFpsState = true;
-        if (isSideFps) {
+        if (!isUdfps) {
             final boolean interactiveToAuthEnabled =
                     mFingerprintInteractiveToAuthProvider != null &&
                             mFingerprintInteractiveToAuthProvider.isEnabled(getCurrentUser());
