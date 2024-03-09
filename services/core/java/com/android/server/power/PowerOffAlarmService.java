5283359912bf25b0c62895dab02f7f1e139b8bdb
Alexander Winkowski <dereference23@outlook.com>
2024-03-09 07:29:56 +0000

    Revert "base: PowerOffAlarmService: Add a notification"
    This reverts commit 28cb7ce5464c96b2ff44b5174883d9c0b4b5804c.
    
    This notification appears far too frequently which makes it more
    annoying than useful.
    
    Change-Id: I1b81dba0c4630a2f7ccb97e09f07dba0544d20a0

--- services/core/java/com/android/server/power/PowerOffAlarmService.java
+++ services/core/java/com/android/server/power/PowerOffAlarmService.java
@@ -15,13 +15,7 @@
  */
 package com.android.server.power;
 
-import static android.provider.AlarmClock.ACTION_SHOW_ALARMS;
-
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -33,7 +27,6 @@ import android.os.Environment;
 import android.os.UserHandle;
 import android.util.Slog;
 
-import com.android.internal.R;
 import com.android.server.SystemService;
 
 import java.io.File;
@@ -56,12 +49,8 @@ public class PowerOffAlarmService extends SystemService {
     private static final String ACTION_CANCEL = "org.codeaurora.poweroffalarm.action.CANCEL_ALARM";
     private static final String ALARM_PACKAGE = "com.qualcomm.qti.poweroffalarm";
 
-    private static final int NOTIFICATION_ID = 0;
-
     private final Context mContext;
     private final AlarmManager mAlarmManager;
-    private NotificationManager mNotificationManager;
-    private Notification mNotification;
     private SharedPreferences mSharedPreferences;
     private boolean mIsAvailable = false;
 
@@ -100,8 +89,6 @@ public class PowerOffAlarmService extends SystemService {
         if (phase != SystemService.PHASE_BOOT_COMPLETED || !mIsAvailable)
             return;
         Slog.v(TAG, "onBootPhase PHASE_BOOT_COMPLETED");
-        mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        setupNotification();
         updateAlarms(mAlarmManager);
     }
 
@@ -109,7 +96,7 @@ public class PowerOffAlarmService extends SystemService {
         @Override
         public void onReceive(Context context, Intent intent) {
             Slog.v(TAG, "mAlarmChangedReceiver onReceive");
-            updateAlarms(mAlarmManager, true);
+            updateAlarms(mAlarmManager);
         }
     };
 
@@ -125,15 +112,10 @@ public class PowerOffAlarmService extends SystemService {
     }
 
     private synchronized void updateAlarms(AlarmManager alarmManager) {
-        updateAlarms(alarmManager, false);
-    }
-
-    private synchronized void updateAlarms(AlarmManager alarmManager, boolean user) {
         final AlarmManager.AlarmClockInfo alarmInfo = alarmManager.getNextAlarmClock();
         cancelPowerOffAlarm();
-        final boolean isSet = alarmInfo != null;
-        if (isSet) setPowerOffAlarm(alarmInfo);
-        updateNotification(isSet && user);
+        if (alarmInfo == null) return;
+        setPowerOffAlarm(alarmInfo);
     }
 
     private synchronized void cancelPowerOffAlarm() {
@@ -158,32 +140,6 @@ public class PowerOffAlarmService extends SystemService {
         Slog.i(TAG, "Set next power off alarm. Time: " + time);
         mContext.sendBroadcastAsUser(getIntent(ACTION_SET, time), UserHandle.SYSTEM);
         sharedPrefs.edit().putLong(PREF_NEXT_ALARM, time).commit();
-    }
-
-    private synchronized void updateNotification(boolean isSet) {
-        mNotificationManager.cancel(NOTIFICATION_ID);
-        if (!isSet) return;
-        mNotificationManager.notify(NOTIFICATION_ID, mNotification);
-    }
-
-    private void setupNotification() {
-        final Intent intent = (new Intent(ACTION_SHOW_ALARMS))
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        final PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, intent,
-                PendingIntent.FLAG_IMMUTABLE);
-        final NotificationChannel channel = new NotificationChannel(TAG /* channel id */,
-                mContext.getText(R.string.notification_channel_poweroff_alarm),
-                NotificationManager.IMPORTANCE_LOW);
-        channel.setBlockable(true);
-        mNotificationManager.createNotificationChannel(channel);
-        mNotification = new Notification.Builder(mContext, TAG /* channel id */)
-                .setContentTitle(mContext.getText(R.string.poweroff_alarm_title))
-                .setContentText(mContext.getText(R.string.poweroff_alarm_body))
-                .setSmallIcon(R.drawable.ic_alarm_on)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .setShowWhen(false)
-                .build();
     }
 
     private static Intent getIntent(String action, long time) {
